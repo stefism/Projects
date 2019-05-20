862fb548d09901d36f68b72e119b1dadec9d74de
stefism <50634763+stefism@users.noreply.github.com>
2019-05-20 20:29:16 +0300

    200519

---
+++ ArraysMoreExercises/MostFrequentNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MostFrequentNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellohghh World!");
+        }
+    }
+}

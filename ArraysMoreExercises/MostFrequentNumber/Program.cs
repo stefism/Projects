4032271f751a47466b0d74eac9a88f6d30e8a947
Stefan Markov <stef4otm@gmail.com>
2019-05-21 12:19:44 +0300

    210519

--- ArraysMoreExercises/MostFrequentNumber/Program.cs
+++ ArraysMoreExercises/MostFrequentNumber/Program.cs
@@ -15,7 +15,7 @@ namespace MostFrequentNumber
 
             for (int i = 0; i < input.Count; i++)
             {
-                for (int j = 1; j < input.Count; j++)
+                for (int j = 1; j < input.Count; j++) 
                 {
                     if (input[i] == input[j])
                     {

1a68d63aaa562874b7112ee82a432827703eba6d
Stefan Markov <stef4otm@gmail.com>
2019-05-21 12:19:21 +0300

    210519

--- ArraysMoreExercises/MostFrequentNumber/Program.cs
+++ ArraysMoreExercises/MostFrequentNumber/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MostFrequentNumber
 {
@@ -6,7 +8,29 @@ namespace MostFrequentNumber
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hellohghh World!");
+            List<int> input = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int repeatCounter = 0;
+            int maxCounter = 0;
+            int repeatNumber = 0;
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                for (int j = 1; j < input.Count; j++)
+                {
+                    if (input[i] == input[j])
+                    {
+                        repeatCounter++;
+                    }
+                }
+
+                if (repeatCounter > maxCounter)
+                {
+                    maxCounter = repeatCounter;
+                    repeatNumber = i;
+                }
+            }
+
+            Console.WriteLine(repeatNumber);
         }
     }
 }

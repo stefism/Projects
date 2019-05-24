5ddb5c6e8676cfac24494dbb848a4ac9e7476c2b
stefism <50634763+stefism@users.noreply.github.com>
2019-05-24 20:43:41 +0300

    240519

---
+++ 02-C#Fundamentals-May2019/08-DataTypesAndVariables/02-PoundsToDollars/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _02_PoundsToDollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pounds = double.Parse(Console.ReadLine());
+            double dollars = pounds * 1.31;
+
+            Console.WriteLine("{0:F3}", dollars);
+        }
+    }
+}

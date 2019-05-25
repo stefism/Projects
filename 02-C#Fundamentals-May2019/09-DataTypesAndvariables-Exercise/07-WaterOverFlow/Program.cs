2085ccd8f699d06ce4799191b5841e27e717ef67
stefism <50634763+stefism@users.noreply.github.com>
2019-05-25 23:06:46 +0300

    250519

---
+++ 02-C#Fundamentals-May2019/09-DataTypesAndvariables-Exercise/07-WaterOverFlow/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _07_WaterOverFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int waterTankCapacity = 255;
+            int currentTankCapacity = 0;
+
+            int substractTimes = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < substractTimes; i++)
+            {
+                int pourLiters = int.Parse(Console.ReadLine());
+
+                currentTankCapacity += pourLiters;
+
+                if (currentTankCapacity > waterTankCapacity)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                    currentTankCapacity -= pourLiters;
+                }
+            }
+
+            Console.WriteLine(currentTankCapacity);
+        }
+    }
+}
---
+++ 02-C#Fundamentals-May2019/09-DataTypesAndvariables-Exercise/11-Snowballs/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Numerics;
+
+namespace _11_Snowballs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int snowballs = int.Parse(Console.ReadLine());
+
+            BigInteger snowballMaxValue = 0;
+
+            BigInteger snowMaxValue = 0;
+            BigInteger timeMaxValue = 0;
+            BigInteger qualityMaxValue = 0;
+
+            for (int i = 0; i < snowballs; i++)
+            {
+                int snowballSnow = int.Parse(Console.ReadLine());
+                int snowballTime = int.Parse(Console.ReadLine());
+                int snowballQuality = int.Parse(Console.ReadLine());
+
+                BigInteger currentSnowballValue = BigInteger.Pow((snowballSnow / snowballTime), snowballQuality);
+
+                if (currentSnowballValue > snowballMaxValue)
+                {
+                    snowballMaxValue = currentSnowballValue;
+                    snowMaxValue = snowballSnow;
+                    timeMaxValue = snowballTime;
+                    qualityMaxValue = snowballQuality;
+                }
+            }
+
+            Console.WriteLine($"{snowMaxValue} : {timeMaxValue} = {snowballMaxValue} ({qualityMaxValue})");
+        }
+    }
+}

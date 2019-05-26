f332b0266b1f336e27292380cf70c4bd82e99373
stefism <50634763+stefism@users.noreply.github.com>
2019-05-26 15:15:45 +0300

    260519

---
+++ 02-C#Fundamentals-May2019/09-DataTypes-MoreExercise/Proba/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Proba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string b = "123";
+            int c = int.Parse(b);
+        }
+    }
+}

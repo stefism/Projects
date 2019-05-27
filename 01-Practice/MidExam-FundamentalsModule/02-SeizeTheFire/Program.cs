c7b7c7f44cec66e146fe0c9c2cbdf9696f4164e5
stefism <50634763+stefism@users.noreply.github.com>
2019-05-28 00:31:11 +0300

    280519

--- 01-Practice/MidExam-FundamentalsModule/02-SeizeTheFire/Program.cs
+++ 01-Practice/MidExam-FundamentalsModule/02-SeizeTheFire/Program.cs
@@ -18,36 +18,93 @@ namespace _02_SeizeTheFire
             double effort = 0;
             double totalFire = 0;
 
-            while (water >= 0)
+            for (int cellInfo = 0; cellInfo < fireAndCell.Count; cellInfo++)
             {
-                for (int cellInfo = 0; cellInfo < fireAndCell.Count; cellInfo++)
-                {
-                    List<string> firePower = fireAndCell[cellInfo].Split("=").ToList();
-                    int firePowerValue = int.Parse(firePower[1]);
-                    string fireType = firePower[0];
+                List<string> firePower = fireAndCell[cellInfo].Split("=").ToList();
+                int firePowerValue = int.Parse(firePower[1]);
+                string fireType = firePower[0].Trim();
 
-                    switch (fireType)
+                if (water <= 0)
+                {
+                    Console.WriteLine("Cells:");
+
+                    foreach (var item in cellValue)
                     {
-                        case "High":
-                            if (firePowerValue >= 81 && firePowerValue <= 125)
-                            {
-                                if (water < firePowerValue)
-                                {
-                                    continue;
-                                }
-                                else
-                                {
-                                    water -= firePowerValue;
-                                    cellValue.Add(firePowerValue);
-                                    effort += firePowerValue * 0.75;
-                                    totalFire += firePowerValue;
-                                }
-                            }
-                            break;
+                        Console.WriteLine($"- {item}");
                     }
 
+                    Console.WriteLine($"Effort: {effort:F2}");
+                    Console.WriteLine($"Total Fire: {totalFire}");
+                    return;
+                }
+
+                switch (fireType)
+                {
+                    case "High":
+                        if (firePowerValue >= 81 && firePowerValue <= 125)
+                        {
+                            if (water < firePowerValue)
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                water -= firePowerValue;
+                                cellValue.Add(firePowerValue);
+                                effort += firePowerValue * 0.25;
+                                totalFire += firePowerValue;
+                                //fireAndCell.RemoveAt(cellInfo);
+                            }
+                        }
+                        break;
+
+                    case "Medium":
+                        if (firePowerValue >= 51 && firePowerValue <= 80)
+                        {
+                            if (water < firePowerValue)
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                water -= firePowerValue;
+                                cellValue.Add(firePowerValue);
+                                effort += firePowerValue * 0.25;
+                                totalFire += firePowerValue;
+                                //fireAndCell.RemoveAt(cellInfo);
+                            }
+                        }
+                        break;
+
+                    case "Low":
+                        if (firePowerValue >= 1 && firePowerValue <= 50)
+                        {
+                            if (water < firePowerValue)
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                water -= firePowerValue;
+                                cellValue.Add(firePowerValue);
+                                effort += firePowerValue * 0.25; 
+                                totalFire += firePowerValue;
+                                //fireAndCell.RemoveAt(cellInfo);
+                            }
+                        }
+                        break;
                 }
             }
+
+            Console.WriteLine("Cells:");
+
+            foreach (var item in cellValue)
+            {
+                Console.WriteLine($"- {item}");
+            }
+
+            Console.WriteLine($"Effort: {effort:F2}");
+            Console.WriteLine($"Total Fire: {totalFire}");
         }
     }
 }

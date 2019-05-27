ac69ceedd34dd0f43e3ed62ecd33631382b593b9
Stefan Markov <stef4otm@gmail.com>
2019-05-27 17:11:42 +0300

    270519

---
+++ 01-Practice/MidExam-FundamentalsModule/02-SeizeTheFire/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02_SeizeTheFire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Technology Fundamentals Mid Exam - 10 March 2019 Group 2
+
+            List<string> fireAndCell = Console.ReadLine().Split("#").ToList();
+            int water = int.Parse(Console.ReadLine());
+
+            List<int> cellValue = new List<int>();
+
+            double effort = 0;
+            double totalFire = 0;
+
+            while (water >= 0)
+            {
+                for (int cellInfo = 0; cellInfo < fireAndCell.Count; cellInfo++)
+                {
+                    List<string> firePower = fireAndCell[cellInfo].Split("=").ToList();
+                    int firePowerValue = int.Parse(firePower[1]);
+                    string fireType = firePower[0];
+
+                    switch (fireType)
+                    {
+                        case "High":
+                            if (firePowerValue >= 81 && firePowerValue <= 125)
+                            {
+                                if (water < firePowerValue)
+                                {
+                                    continue;
+                                }
+                                else
+                                {
+                                    water -= firePowerValue;
+                                    cellValue.Add(firePowerValue);
+                                    effort += firePowerValue * 0.75;
+                                    totalFire += firePowerValue;
+                                }
+                            }
+                            break;
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ 02-C#Fundamentals-May2019/09-DataTypes-MoreExercise/04-RefacrotingPrimeChecker/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _04_RefacrotingPrimeChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+            for (int number = 2; number <= inputNumber; number++)
+            {
+                bool isPrime = true;
+                for (int currentNumber = 2; currentNumber < number; currentNumber++)
+                {
+                    if (number % currentNumber == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                Console.WriteLine("{0} -> {1}", number, isPrime.ToString().ToLower());
+            }
+        }
+    }
+}

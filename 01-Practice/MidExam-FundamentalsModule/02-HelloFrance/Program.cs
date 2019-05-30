ed604e026b4ef91386cb4ae1e084081c26e7d7a5
Stefan Markov <stef4otm@gmail.com>
2019-05-30 11:56:10 +0300

    300519

--- 01-Practice/MidExam-FundamentalsModule/02-HelloFrance/Program.cs
+++ 01-Practice/MidExam-FundamentalsModule/02-HelloFrance/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _02_HelloFrance
 {
@@ -9,7 +11,72 @@ namespace _02_HelloFrance
 
             // Technology Fundamentals Mid Exam - 10 March 2019 Group 1
 
-            Console.WriteLine("Hello World!");
+            List<string> inputArticlesAndPrice = Console.ReadLine().Split("|").ToList();
+            double initialBudget = double.Parse(Console.ReadLine());
+
+            double leftBudget = initialBudget;
+            double totalProfit = 0;
+
+            List<double> newPrices = new List<double>();
+
+            for (int i = 0; i < inputArticlesAndPrice.Count; i++)
+            {
+                List<string> articlesAndPrice = inputArticlesAndPrice[i].Split("->").ToList();
+
+                string articles = articlesAndPrice[0];
+                double buySum = double.Parse(articlesAndPrice[1]);
+
+                switch (articles)
+                {
+                    case "Clothes":
+                        if (buySum <= 50 && leftBudget >= buySum)
+                        {
+                            double sumWithProfit = buySum * 1.40;
+                            newPrices.Add(sumWithProfit);
+                            totalProfit += sumWithProfit - buySum;
+                            leftBudget -= buySum;
+                        }
+                        break;
+
+                    case "Shoes":
+                        if (buySum <= 35 && leftBudget >= buySum)
+                        {
+                            double sumWithProfit = buySum * 1.40;
+                            newPrices.Add(sumWithProfit);
+                            totalProfit += sumWithProfit - buySum;
+                            leftBudget -= buySum;
+                        }
+                        break;
+
+                    case "Accessories":
+                        if (buySum <= 20.50 && leftBudget >= buySum)
+                        {
+                            double sumWithProfit = buySum * 1.40;
+                            newPrices.Add(sumWithProfit);
+                            totalProfit += sumWithProfit - buySum;
+                            leftBudget -= buySum;
+                        }
+                        break;
+                }
+            }
+
+            for (int i = 0; i < newPrices.Count; i++)
+            {
+                Console.Write("{0:F2}", newPrices[i]);
+                Console.Write(" ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Profit: {totalProfit:F2}");
+
+            if (totalProfit+initialBudget >= 150)
+            {
+                Console.WriteLine("Hello, France!");
+            }
+            else
+            {
+                Console.WriteLine("Time to go.");
+            }
         }
     }
 }

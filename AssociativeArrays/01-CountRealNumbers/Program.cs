d4af52e9ce8a7f273ddd4e40cbd0b299b3ae7d40
Stefan Markov <stef4otm@gmail.com>
2019-06-19 15:18:01 +0300

    190619

---
+++ AssociativeArrays/01-CountRealNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01_CountRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> input = Console.ReadLine().Split().Select(double.Parse).ToList();
+
+            var counts = new SortedDictionary<double, int>();
+
+            foreach (var item in input)
+            {
+                if (!counts.ContainsKey(item))
+                {
+                    counts[item] = 1;
+                }
+                else
+                {
+                    counts[item]++;
+                }
+            }
+
+            foreach (var item in counts)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
---
+++ AssociativeArrays/03-WordSynonyms/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03_WordSynonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<string>> dictionary = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+
+                if (!dictionary.ContainsKey(word))
+                {
+                    dictionary[word] = new List<string>(); //  или dictionary.Add(word, new List<string>());
+                }
+
+                dictionary[word].Add(synonym);
+            }
+        }
+    }
+}

1505e37a6295f3ec3015e2c0b5b5b7164e31e989
Stefan Markov <stef4otm@gmail.com>
2019-06-28 16:04:09 +0300

    280619

---
+++ 02-C#Fundamentals-May2019/21-ObjAndClasses-Exercises/04-RawData/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04_RawData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCars = int.Parse(Console.ReadLine());
+            List<Cars> allCars = new List<Cars>();
+
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                List<string> currentCarInfo = Console.ReadLine().Split().ToList();
+
+                string carModel = currentCarInfo[0];
+                double engineSpeed = double.Parse(currentCarInfo[1]);
+                double enginePower = double.Parse(currentCarInfo[2]);
+                double cargoWeight = double.Parse(currentCarInfo[3]);
+                string cargoType = currentCarInfo[4];
+
+                Cars currentCar = new Cars(carModel, engineSpeed, enginePower, cargoWeight, cargoType);
+
+                allCars.Add(currentCar);
+            }
+
+            string cargo = Console.ReadLine();
+
+            foreach (var item in allCars)
+            {
+                if (cargo == "fragile")
+                {
+                    if (item.CargoType == "fragile" && item.CargoWeight < 1000)
+                    {
+                        Console.WriteLine(item.Model);
+                    }
+                }
+                else
+                {
+                    if (item.CargoType == "flamable" && item.EnginePower > 250)
+                    {
+                        Console.WriteLine(item.Model);
+                    }
+                }
+            }
+
+        }
+    }
+    class Cars
+    {
+        public string Model { get; set; }
+        public double EngineSpeed { get; set; }
+        public double EnginePower { get; set; }
+        public double CargoWeight { get; set; }
+        public string CargoType { get; set; }
+
+        public Cars(string model, double engineSpeed, double enginePower, double cargoWeght, string cargoType)
+        {
+            this.Model = model;
+            this.EngineSpeed = engineSpeed;
+            this.EnginePower = enginePower;
+            this.CargoWeight = cargoWeght;
+            this.CargoType = cargoType;
+
+        }
+    }
+}
---
+++ 02-C#Fundamentals-May2019/21-ObjAndClasses-Exercises/05m-Shopping Spree/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05m_Shopping_Spree
+{
+
+    class Program
+    {
+        public static List<PeoplesAndProducts> ByedItemsForPrint = new List<PeoplesAndProducts>();
+        static void Main(string[] args)
+        {
+            List<string> inputPersons = Console.ReadLine().Split(";", StringSplitOptions.RemoveEmptyEntries).ToList();
+            List<string> splitedPerson = new List<string>();
+
+            List<string> inputProducts = Console.ReadLine().Split(";", StringSplitOptions.RemoveEmptyEntries).ToList();
+            List<string> splitedProduct = new List<string>();
+
+            List<Person> persons = new List<Person>();
+            List<Product> products = new List<Product>();
+
+            persons = CreatePersonsList(inputPersons, splitedPerson, persons);
+            products = CreateProductList(inputProducts, splitedProduct, products);
+
+            while (true)
+            {
+                List<string> command = Console.ReadLine().Split().ToList();
+
+                if (command[0] == "END")
+                {
+                   
+                    break;
+                }
+
+                BuyProductOrNot(persons, command, products);
+
+            }
+        }
+        static void AddProductToList(List<PeoplesAndProducts> byedItemsForPrint, string personName, string productName)
+        {
+            foreach (var item in byedItemsForPrint)
+            {
+                if (item.PeopleName == personName)
+                {
+                    PeoplesAndProducts product = new PeoplesAndProducts();
+                    product.BuyedProducts.Add(productName); 
+                }
+            }
+        }
+        static void BuyProductOrNot(List<Person> persons, List<string> command, List<Product> products)
+        {
+            string personName = command[0];
+            string productName = command[1];
+
+            foreach (var item in persons)
+            {
+                if (item.PersonName == personName)
+                {
+                    double productPrice = ReturnProductPrice(productName, products);
+
+                    if (item.PersonMoney >= productPrice)
+                    {
+                        Console.WriteLine($"{item.PersonName} bought {productName}");
+                        item.PersonMoney -= productPrice;
+                        PeoplesAndProducts currentProduct = new PeoplesAndProducts();
+                        currentProduct.PeopleName = item.PersonName;
+                        currentProduct.BuyedProducts.Add(productName);
+                        ByedItemsForPrint.Add(currentProduct);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{item.PersonName} can't afford {productName}");
+                    }
+                }
+            }
+        }
+        static double ReturnProductPrice(string productName, List<Product> products)
+        {
+            double productPrice = 0;
+
+            foreach (var item in products)
+            {
+                if (item.ProductName == productName)
+                {
+                    productPrice = item.ProductCost;
+                    return productPrice;
+                }
+            }
+            return productPrice;
+        }
+
+        static List<Product> CreateProductList(List<string> inputProducts, List<string> splitedProduct, List<Product> outputProduct)
+        {
+            for (int i = 0; i < inputProducts.Count; i++)
+            {
+                Product product = new Product();
+                splitedProduct = inputProducts[i].Split("=").ToList();
+                product.ProductName = splitedProduct[0];
+                product.ProductCost = double.Parse(splitedProduct[1]);
+                outputProduct.Add(product);
+            }
+            return outputProduct;
+        }
+
+        static List<Person> CreatePersonsList(List<string> inputPerson, List<string> splitedPerson, List<Person> outputPerson)
+        {
+            for (int i = 0; i < inputPerson.Count; i++)
+            {
+                Person person = new Person();
+                splitedPerson = inputPerson[i].Split("=").ToList();
+                person.PersonName = splitedPerson[0];
+                person.PersonMoney = double.Parse(splitedPerson[1]);
+                outputPerson.Add(person);
+            }
+            return outputPerson;
+        }
+    }
+
+    class PeoplesAndProducts
+    {
+        public string PeopleName { get; set; }
+        public List<string> BuyedProducts { get; set; }
+
+        public PeoplesAndProducts()
+        {
+            BuyedProducts = new List<string>();
+        }
+    }
+
+    class Person
+    {
+        public string PersonName { get; set; }
+        public double PersonMoney { get; set; }
+    }
+
+    class Product
+    {
+        public string ProductName { get; set; }
+        public double ProductCost { get; set; }
+    }
+}

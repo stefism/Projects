a4c0bd125e64c03af59fdacf51ce9b49e3274958
Stefan Markov <stef4otm@gmail.com>
2019-07-09 23:41:04 +0300

    090719

---
+++ 02-C#Fundamentals-May2019/26-AssociativeArrays-Exercise/Samples/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var someDict = new Dictionary<char, List<string>>();
+            someDict.Add('a', new List<string>() { "Gosho", "Ivan"});
+        }
+    }
+}

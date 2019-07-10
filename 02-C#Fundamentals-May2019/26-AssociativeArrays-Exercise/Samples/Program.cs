5d65754e79f99fb5478eae28d2fefc32206bbec4
Stefan Markov <stef4otm@gmail.com>
2019-07-10 13:10:11 +0300

    100719

--- 02-C#Fundamentals-May2019/26-AssociativeArrays-Exercise/Samples/Program.cs
+++ 02-C#Fundamentals-May2019/26-AssociativeArrays-Exercise/Samples/Program.cs
@@ -9,7 +9,21 @@ namespace Samples
         static void Main(string[] args)
         {
             var someDict = new Dictionary<char, List<string>>();
-            someDict.Add('a', new List<string>() { "Gosho", "Ivan"});
+            someDict.Add('a', new List<string>() { "Gosho"});
+            someDict.Add('b', new List<string>() { "Dimitar", "Petar"});
+            someDict.Add('c', new List<string>() { "Ivan"});
+            someDict.Add('d', new List<string>() { "Dragan" });
+
+            List<string> names = someDict['c'];
+
+            //string element = names[5];
+
+            someDict = someDict.Where(x => x.Value.Count > 1)
+                .OrderBy(x => x.Value)
+                .ToDictionary(x => x.Key, y => y.Value);
+
+            Console.WriteLine(string.Join(Environment.NewLine, someDict
+                .Select(x => $"{x.Key}: - {string.Join(", ", x.Value)}")));
         }
     }
 }

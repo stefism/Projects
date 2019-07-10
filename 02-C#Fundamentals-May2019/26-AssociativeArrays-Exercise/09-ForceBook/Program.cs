d60f379a15b6a7f4d489da38a596b68d21294893
Stefan Markov <stef4otm@gmail.com>
2019-07-10 22:18:16 +0300

    100719

---
+++ 02-C#Fundamentals-May2019/26-AssociativeArrays-Exercise/09-ForceBook/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09_ForceBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sides = new Dictionary<string, List<string>>();
+
+            while (true)
+            {
+                string commands = Console.ReadLine();
+
+                if (commands == "Lumpawaroo")
+                {
+                    sides = sides.OrderByDescending(x => x.Value.Count)
+                        .ThenBy(x => x.Key)
+                        .ToDictionary(x => x.Key, x => x.Value);
+
+                    foreach (var item in sides)
+                    {
+                        item.Value.Sort();
+                    }    
+
+                    foreach (var item in sides)
+                    {
+                        if (item.Value.Count > 0)
+                        {
+                            Console.WriteLine($"Side: {item.Key}, Members: {item.Value.Count}");
+                            Console.WriteLine("! " + string.Join(Environment.NewLine + "! ", item.Value));
+                        }
+                    }
+                    break;
+                }
+
+                if (commands.Contains("|"))
+                {
+                    string[] splitedCommands = commands.Split(" | ");
+                    string side = splitedCommands[0];
+                    string name = splitedCommands[1];
+
+                    if (!sides.ContainsKey(side))
+                    {
+                        sides[side] = new List<string>();
+                    }
+
+                    sides[side].Add(name);
+                }
+
+                else if (commands.Contains("->"))
+                {
+                    string[] splitedCommands = commands.Split(" -> ");
+                    string name = splitedCommands[0];
+                    string side = splitedCommands[1];
+
+                    foreach (var item in sides)
+                    {
+                        if (item.Value.Contains(name))
+                        {
+                            item.Value.Remove(name);
+                            break;
+                        }
+                    }
+
+                    if (sides.ContainsKey(side))
+                    {
+                        sides[side].Add(name);
+                        Console.WriteLine($"{name} joins the {side} side!");
+                    }
+                }
+            }
+        }
+    }
+}

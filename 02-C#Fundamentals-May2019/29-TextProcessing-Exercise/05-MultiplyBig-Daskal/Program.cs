baf2f093a84e1623f1ea8b2eab45d8a98f5012b0
Stefan Markov <stef4otm@gmail.com>
2019-07-15 19:04:19 +0300

    150719

---
+++ 02-C#Fundamentals-May2019/29-TextProcessing-Exercise/05-MultiplyBig-Daskal/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _05_MultiplyBig_Daskal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numberAsString = Console.ReadLine();
+            int multiplier = int.Parse(Console.ReadLine());
+
+            var builder = new StringBuilder();
+
+            for (int i = numberAsString.Length-1; i >= 0; i--)
+            {
+                int lastDigit = int.Parse(numberAsString[i].ToString());
+
+                int result = lastDigit * multiplier;
+
+                builder.Append(result);
+            }
+        }
+    }
+}

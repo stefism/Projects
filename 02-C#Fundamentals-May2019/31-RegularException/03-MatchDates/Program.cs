5638f6943575c68f5807eca0def26a5356094303
Stefan Markov <stef4otm@gmail.com>
2019-07-19 13:50:24 +0300

    190719

--- 02-C#Fundamentals-May2019/31-RegularException/03-MatchDates/Program.cs
+++ 02-C#Fundamentals-May2019/31-RegularException/03-MatchDates/Program.cs
@@ -7,17 +7,22 @@ namespace _03_MatchDates
     {
         static void Main(string[] args)
         {
-            var regex = new Regex(@"\b(?<day>\d{2})([-.\/])(?<month>[A-Z][a-z]{2})\2(?<year>\d{4})\b");
+            var regex = new Regex(@"\b(\d{2})([-.\/])([A-Z][a-z]{2})\2(\d{4})\b");
+
+            // @"\b(\d{2})([-.\/])([A-Z][a-z]{2})\2(\d{4})\b" - Работи.
+            // \b(?<day>\d{2})([-.\/])(?<month>[A-Z][a-z]{2})\2(?<year>\d{4})\b - Не работи.
 
             string dates = Console.ReadLine();
 
             var matches = regex.Matches(dates);
 
-            Console.WriteLine(string.Join(", ", matches));
+            //Console.WriteLine(string.Join(", ", matches));
 
             foreach (Match item in matches)
             {
-                Console.WriteLine(item.Groups["day"].Value);
+                Console.WriteLine($"Day: {item.Groups["1"].Value}, Month: {item.Groups["3"].Value}, Year: {item.Groups["4"].Value}");
+                //Console.WriteLine(item.Groups["3"].Value);
+                //Console.WriteLine(item.Groups["4"].Value);
             }
         }
     }

1324bbaf7721399539bbfe75f5880a36fd136e56
Stefan Markov <stef4otm@gmail.com>
2019-07-22 17:23:02 +0300

    220719

---
+++ 02-C#Fundamentals-May2019/31-RegularException/01e-Furniture/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01e_Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // >>([A-Z]+[a-z]+|[A-Z]+)<<(\d+\.?\d+)!(\d+)
+            // >>(?<name>[A-Z]+[a-z]+|[A-Z]+)<<(?<price>\d+\.?\d+)!(?<quantity>\d+)
+
+            var regex = new Regex(@">>(?<name>[A-Z]+[a-z]+|[A-Z]+)<<(?<price>\d+\.?\d+)!(?<quantity>\d+)");
+
+            while (true)
+            {
+                string currentInfo = Console.ReadLine();
+
+                if (currentInfo == "Purchase")
+                {
+                    Console.WriteLine("Bought furniture:");
+                }
+
+                var matches = regex.Matches(currentInfo);
+                
+
+            }
+        }
+    }
+}
--- 02-C#Fundamentals-May2019/31-RegularException/03-MatchDates/Program.cs
+++ 02-C#Fundamentals-May2019/31-RegularException/03-MatchDates/Program.cs
@@ -7,7 +7,7 @@ namespace _03_MatchDates
     {
         static void Main(string[] args)
         {
-            var regex = new Regex(@"\b(\d{2})([-.\/])([A-Z][a-z]{2})\2(\d{4})\b");
+            var regex = new Regex(@"\b(?<day>\d{2})([-.\/])(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b");
 
             // @"\b(\d{2})([-.\/])([A-Z][a-z]{2})\2(\d{4})\b" - Работи.
             // \b(?<day>\d{2})([-.\/])(?<month>[A-Z][a-z]{2})\2(?<year>\d{4})\b - Не работи.
@@ -20,7 +20,7 @@ namespace _03_MatchDates
 
             foreach (Match item in matches)
             {
-                Console.WriteLine($"Day: {item.Groups["1"].Value}, Month: {item.Groups["3"].Value}, Year: {item.Groups["4"].Value}");
+                Console.WriteLine($"Day: {item.Groups["day"].Value}, Month: {item.Groups["month"].Value}, Year: {item.Groups["year"].Value}");
                 //Console.WriteLine(item.Groups["3"].Value);
                 //Console.WriteLine(item.Groups["4"].Value);
             }

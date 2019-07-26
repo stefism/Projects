edb457078b6baf6c10410e48fed2115ba84d4bc0
Stefan Markov <stef4otm@gmail.com>
2019-07-26 17:20:12 +0300

    260719

--- 02-C#Fundamentals-May2019/FinalExamPreparation240719/01-Concert/Program.cs
+++ 02-C#Fundamentals-May2019/FinalExamPreparation240719/01-Concert/Program.cs
@@ -12,52 +12,78 @@ namespace _01_Concert
             var bandsInfo = new Dictionary<string, List<string>>();
             var bandsPlayTime = new Dictionary<string, int>();
 
+            int totalPlayTime = 0;
+
             while (true)
             {
-                string[] input = Console.ReadLine().Split(";");
+                string[] input = Console.ReadLine().Split("; ");
 
                 string command = input[0];
 
                 if (command == "start of concert")
                 {
+                    bandsPlayTime = bandsPlayTime.OrderByDescending(x => x.Value)
+                        .ThenBy(x => x.Key)
+                        .ToDictionary(x => x.Key, x => x.Value);
+
+                    Console.WriteLine($"Total time: {totalPlayTime}");
+
+                    Console.WriteLine(string.Join(Environment.NewLine, bandsPlayTime
+                        .Select(x => $"{x.Key} -> {x.Value}")));
 
                     break;
                 }
 
                 else if (command == "Play")
                 {
-                    string bandName = input[1];
+                    string bandNamePlay = input[1];
                     int bandTime = int.Parse(input[2]);
 
-                    if (!bandsPlayTime.ContainsKey(bandName))
+                    if (!bandsPlayTime.ContainsKey(bandNamePlay))
                     {
-                        bandsPlayTime[bandName] = 0;
+                        bandsPlayTime[bandNamePlay] = 0;
                     }
 
-                    bandsPlayTime[bandName] += bandTime;
+                    bandsPlayTime[bandNamePlay] += bandTime;
+                    totalPlayTime += bandTime;
                 }
 
                 else if (command == "Add")
                 {
-                    string bandName = input[1];
+                    string bandNameAdd = input[1];
                     List<string> bandsMembers = input[2].Split(", ").ToList();
 
-                    if (!bandsInfo.ContainsKey(bandName))
+                    if (!bandsInfo.ContainsKey(bandNameAdd))
                     {
-                        bandsInfo[bandName] = new List<string>(bandsMembers);
+                        bandsInfo[bandNameAdd] = new List<string>(bandsMembers);
                     }
                     else
                     {
                         foreach (var member in bandsMembers)
                         {
-                            if (!bandsInfo[bandName].Contains(member))
+                            if (!bandsInfo[bandNameAdd].Contains(member))
                             {
-                                bandsInfo[bandName].Add(member);
+                                bandsInfo[bandNameAdd].Add(member);
                             }
                         }
                     }
                 }
             }
+
+            string bandName = Console.ReadLine();
+
+            Console.WriteLine(bandName);
+
+            foreach (var item in bandsInfo)
+            {
+                if (item.Key == bandName)
+                {
+                    Console.WriteLine($"=> {string.Join(Environment.NewLine + "=> ",item.Value)}");
+                }
+            }
+            
+            //Console.WriteLine(string.Join(Environment.NewLine, bandsInfo
+            //    .Select(x => $"=> {x.Value}")));
         }
     }
 }
---
+++ 02-C#Fundamentals-May2019/FinalExamPreparation240719/02-SongEncryption/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _02_SongEncryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // [A-Z]\s*[\s*'?a-z]*:
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

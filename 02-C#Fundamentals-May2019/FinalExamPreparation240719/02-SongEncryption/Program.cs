02764f2c2555d0b3115e5d0d357a478de0995aad
Stefan Markov <stef4otm@gmail.com>
2019-07-26 22:22:08 +0300

    260719

--- 02-C#Fundamentals-May2019/FinalExamPreparation240719/02-SongEncryption/Program.cs
+++ 02-C#Fundamentals-May2019/FinalExamPreparation240719/02-SongEncryption/Program.cs
@@ -10,9 +10,79 @@ namespace _02_SongEncryption
     {
         static void Main(string[] args)
         {
-            // [A-Z]\s*[\s*'?a-z]*:
+            // (?<artist>[A-Z]\s*[\s*'?a-z]*):
+            // :(?<song>[\s*A-Z]*)
 
-            Console.WriteLine("Hello World!");
+            var artistRegex = new Regex(@"^(?<artist>[A-Z]\s*[\s*'?a-z]*):");
+            var songRegex = new Regex(@":(?<song>[\s*A-Z]*)");
+
+            while (true)
+            {
+                string inputAsString = Console.ReadLine();
+
+                if (inputAsString == "end")
+                {
+                    break;
+                }
+
+                var artistMatch = artistRegex.Match(inputAsString);
+                var songMatch = songRegex.Match(inputAsString);
+
+                if (artistMatch.Success && songMatch.Success)
+                {
+                    string[] input = inputAsString.Split(":");
+
+                    string artist = input[0];
+                    string song = input[1]; // ?
+
+                    int key = artist.Length;
+
+                    string output = string.Empty;
+
+                    for (int i = 0; i < inputAsString.Length; i++)
+                    {
+                        if (inputAsString[i] == ':')
+                        {
+                            output += '@';
+                            continue;
+                        }
+
+                        else if (inputAsString[i] == ' ')
+                        {
+                            output += ' ';
+                            continue;
+                        }
+
+                        else if (inputAsString[i] == '\'')
+                        {
+                            output += '\'';
+                            continue;
+                        }
+
+                        if (inputAsString[i] >= 97 && inputAsString[i] <= 122 && key + inputAsString[i] > 122)
+                        {
+                            output += (char)((key + inputAsString[i]) - 26);
+                        }
+
+                        else if (inputAsString[i] >= 65 && inputAsString[i] <= 90 && key + inputAsString[i] > 90)
+                        {
+                            output += (char)((key + inputAsString[i]) - 26);
+                        }
+
+                        else
+                        {
+                            output += (char)(inputAsString[i] + key);
+                        }
+                    }
+
+                    Console.WriteLine($"Successful encryption: {output}");
+                }
+
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+            }
         }
     }
 }

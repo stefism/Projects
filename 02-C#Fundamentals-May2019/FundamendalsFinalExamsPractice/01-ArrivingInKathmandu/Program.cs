ccc1ab120e422551cf4c545b707790dc08370484
Stefan Markov <stef4otm@gmail.com>
2019-07-31 23:15:06 +0300

    310719

--- 02-C#Fundamentals-May2019/FundamendalsFinalExamsPractice/01-ArrivingInKathmandu/Program.cs
+++ 02-C#Fundamentals-May2019/FundamendalsFinalExamsPractice/01-ArrivingInKathmandu/Program.cs
@@ -23,7 +23,7 @@ namespace _01_ArrivingInKathmandu
                 string[] peakAndCodeInDigit = input.Split("=");
                 int geohachcodeDigit = -2;
 
-                if (peakAndCodeInDigit.Length == 0)
+                if (peakAndCodeInDigit.Length == 1)
                 {
                     Console.WriteLine("Nothing found!");
                     continue;
@@ -54,16 +54,36 @@ namespace _01_ArrivingInKathmandu
                     continue;
                 }
 
+
+
                 string encodedPeak = peakAndCodeInDigit[0];
 
                 string decodedPeak = string.Empty;
 
+                bool isPeakNameValid = true;
+
                 for (int i = 0; i < encodedPeak.Length; i++)
                 {
-                    if (char.IsLetter(encodedPeak[i]))
+                    if (!char.IsLetterOrDigit(encodedPeak[i]) && encodedPeak[i] != '!' &&
+                        encodedPeak[i] != '@' && encodedPeak[i] != '#' && encodedPeak[i] != '$'
+                        && encodedPeak[i] != '?')
                     {
-                        decodedPeak += (char)encodedPeak[i];
+                        isPeakNameValid = false;
+                        break;
                     }
+                    else
+                    {
+                        if (char.IsLetterOrDigit(encodedPeak[i]))
+                        {
+                            decodedPeak += (char)encodedPeak[i];
+                        }
+                    }
+                }
+
+                if (!isPeakNameValid)
+                {
+                    Console.WriteLine("Nothing found!");
+                    continue;
                 }
 
                 Console.WriteLine($"Coordinates found! {decodedPeak} -> {geohashcode}");

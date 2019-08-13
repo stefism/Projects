6f880281768057d037019fc600967335b5bd1773
Stefan Markov <stef4otm@gmail.com>
2019-08-13 17:21:08 +0300

    130819

---
+++ C#Advanced-May-2019/04-StacksAndQueues-Exercise/10-Crossroad-v2/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10_Crossroad_v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int greenLight = int.Parse(Console.ReadLine());
+            int freeWindow = int.Parse(Console.ReadLine());
+            int initialGreenLight = greenLight;
+
+            Queue<string> waitingCars = new Queue<string>();
+
+            int passedCarsCount = 0;
+
+            //bool isSave = true;
+
+            while (true)
+            {
+                string car = Console.ReadLine();
+
+                if (car == "END")
+                {
+                    break;
+                }
+
+                if (car != "green")
+                {
+                    waitingCars.Enqueue(car);
+                }
+
+                else
+                {
+                    greenLight = initialGreenLight;
+
+                    while (waitingCars.Count > 0)
+                    {
+                        string currentCar = waitingCars.Dequeue();
+
+                        if (currentCar.Length < greenLight)
+                        {
+                            passedCarsCount++;
+
+                            if (freeWindow == 0)
+                            {
+                                break;
+                            }
+
+                            greenLight -= currentCar.Length;
+
+                            continue;
+                        }
+                        else
+                        {
+                            //greenLight -= currentCar.Length;
+
+                            if (greenLight <= 0)
+                            {
+                                waitingCars.Enqueue(currentCar);
+                                break;
+                            }
+                            else
+                            {
+                                if (currentCar.Length < freeWindow)
+                                {
+                                    passedCarsCount++;
+                                    break;
+                                    //continue;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("A crash happened!");
+
+                                    int crashIndex = freeWindow + greenLight;
+
+                                    if (crashIndex > currentCar.Length-1)
+                                    {
+                                        crashIndex = freeWindow;
+                                    }
+
+                                    Console.WriteLine($"{currentCar} was hit at {currentCar[crashIndex]}.");
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Everyone is safe.");
+            Console.WriteLine($"{passedCarsCount} total cars passed the crossroads.");
+        }
+    }
+}
--- C#Advanced-May-2019/04-StacksAndQueues-Exercise/10-Crossroad/Program.cs
+++ C#Advanced-May-2019/04-StacksAndQueues-Exercise/10-Crossroad/Program.cs
@@ -15,52 +15,51 @@ namespace _10_Crossroad
 
             int passedCarsCount = 0;
 
-            string cars;
-
             bool isSave = true;
 
-            while ((cars = Console.ReadLine()) != "END")
+            while (true)
             {
+                if (!isSave)
+                {
+                    break;
+                }
+
+                string cars = Console.ReadLine();
+
+                if (cars == "END")
+                {
+                    break;
+                }
+
                 isSave = true;
 
                 if (cars == "green")
                 {
-                    string carToPass = waitingCars.Dequeue();
-
-                    if (carToPass.Length < greenLight)
+                    while (waitingCars.Count != 0)
                     {
-                        passedCarsCount++;
+                        string carToPass = waitingCars.Dequeue();
 
-                        int secondLeft = greenLight - carToPass.Length;
-
-                        while (secondLeft != 0)
+                        if (carToPass.Length < greenLight)
                         {
-                            carToPass = waitingCars.Dequeue();
+                            passedCarsCount++;
+                            greenLight -= carToPass.Length;
 
-                            if (carToPass.Length < secondLeft)
+                            if (greenLight > 0)
                             {
-                                passedCarsCount++;
-                                secondLeft -= carToPass.Length;
+                                continue;
                             }
                             else
                             {
-                                if (carToPass.Length - secondLeft < freeWindow)
-                                {
-                                    passedCarsCount++;
-                                }
-                                else
-                                {
-                                    char indexToHit = carToPass[carToPass.Length - secondLeft];
-
-                                    Console.WriteLine("A crash happened!");
-                                    Console.WriteLine($"{carToPass} was hit at {indexToHit}");
-                                    isSave = false;
-                                    break;
-                                }
+                                CalculatePassedCars(greenLight, freeWindow, waitingCars, ref passedCarsCount, ref isSave, ref carToPass);
                             }
+                        }
+                        else
+                        {
+                            CalculatePassedCars(greenLight, freeWindow, waitingCars, ref passedCarsCount, ref isSave, ref carToPass);
                         }
                     }
                 }
+
                 else
                 {
                     waitingCars.Enqueue(cars);
@@ -69,7 +68,49 @@ namespace _10_Crossroad
 
             if (isSave)
             {
+                Console.WriteLine("Everyone is safe.");
                 Console.WriteLine($"{passedCarsCount} total cars passed the crossroads.");
+            }
+        }
+
+        private static void CalculatePassedCars(int greenLight, int freeWindow, Queue<string> waitingCars, ref int passedCarsCount, ref bool isSave, ref string carToPass)
+        {
+            if (waitingCars.Count > 0)
+            {
+                int secondLeft = greenLight - carToPass.Length;
+
+                while (secondLeft != 0)
+                {
+                    if (!isSave)
+                    {
+                        break;
+                    }
+
+                    carToPass = waitingCars.Dequeue();
+
+                    if (carToPass.Length < secondLeft)
+                    {
+                        passedCarsCount++;
+                        secondLeft -= carToPass.Length;
+                    }
+                    else
+                    {
+                        if (carToPass.Length - secondLeft < freeWindow)
+                        {
+                            passedCarsCount++;
+                            break;
+                        }
+                        else
+                        {
+                            char indexToHit = carToPass[carToPass.Length - secondLeft - 1];
+
+                            Console.WriteLine("A crash happened!");
+                            Console.WriteLine($"{carToPass} was hit at {indexToHit}.");
+                            isSave = false;
+                            break;
+                        }
+                    }
+                }
             }
         }
     }

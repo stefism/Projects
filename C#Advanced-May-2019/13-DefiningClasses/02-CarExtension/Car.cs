9658c79aca968d354b92428fbcaef1c535617e7f
Stefan Markov <stef4otm@gmail.com>
2019-08-31 11:22:18 +0300

    310819

---
+++ C#Advanced-May-2019/13-DefiningClasses/02-CarExtension/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02_CarExtension
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+
+        public void Drive(double distance)
+        {
+            double expenceFuel = FuelConsumption * distance;
+
+            if (expenceFuel > FuelQuantity)
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+            else
+            {
+                FuelQuantity -= distance * FuelConsumption;
+            }
+        }
+
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:F2}L";
+        }
+    }
+}

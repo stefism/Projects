d81140de3235e6a9d9d83c5e8104a77d275fa779
Stefan Markov <stef4otm@gmail.com>
2019-09-01 17:06:53 +0300

    010919

---
+++ C#Advanced-May-2019/13-DefiningClasses/03-CarConstructors/Car.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year) : this ()
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public Car(string make, string model, int year, 
+            double fuelQuantity, double fuelConsumption) : this (make, model, year)
+        {
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
+        }
+
+        public void Drive(double distance)
+        {
+            double expenceFuel = FuelConsumption * distance;
+
+            if (expenceFuel > FuelQuantity)
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+            else
+            {
+                FuelQuantity -= distance * FuelConsumption;
+            }
+        }
+
+        public string WhoAmI()
+        {
+            string output = $"Make: {Make}{Environment.NewLine}Model: {Model}{Environment.NewLine}Year: {Year}{Environment.NewLine}Fuel: {FuelQuantity:F2}L";
+            return output;
+        }
+    }
+}

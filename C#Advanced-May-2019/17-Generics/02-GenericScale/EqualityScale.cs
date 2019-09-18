777680e9a78f4b454764a7ce15ae2088b54071b7
Stefan Markov <stef4otm@gmail.com>
2019-09-18 17:21:47 +0300

    180919

---
+++ C#Advanced-May-2019/17-Generics/02-GenericScale/EqualityScale.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+    class EqualityScale<T>
+    {
+        private T left;
+        private T right;
+
+        public bool AreEqual()
+        {
+            return left.Equals(right);
+        }
+    }
+}

450e44957552ce9869dfbbf10b9366591000ff82
Stefan Markov <stef4otm@gmail.com>
2019-09-19 17:19:47 +0300

    190919

--- C#Advanced-May-2019/17-Generics/02-GenericScale/EqualityScale.cs
+++ C#Advanced-May-2019/17-Generics/02-GenericScale/EqualityScale.cs
@@ -4,10 +4,16 @@ using System.Text;
 
 namespace GenericScale
 {
-    class EqualityScale<T>
+    public class EqualityScale<T>
     {
         private T left;
         private T right;
+
+        public EqualityScale(T left, T right)
+        {
+            this.left = left;
+            this.right = right;
+        }
 
         public bool AreEqual()
         {
---
+++ C#Advanced-May-2019/18-Generics-Exercise/01-GenericBoxOfString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _01_GenericBoxOfString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int numberOfInput = int.Parse(Console.ReadLine());
+
+            var box = new Box<string>();
+
+            for (int i = 0; i < numberOfInput; i++)
+            {
+                var input =Console.ReadLine();
+
+                box.Add(input);
+            }
+
+            var stringToCompare = Console.ReadLine();
+
+            int count = box.CountThatGreaterByValue(stringToCompare);
+
+            Console.WriteLine(count);
+        }
+    }
+}

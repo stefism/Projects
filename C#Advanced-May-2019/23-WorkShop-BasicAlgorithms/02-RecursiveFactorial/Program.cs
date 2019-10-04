49277867df5460986ab458be6e5adb9567472582
Stefan Markov <stef4otm@gmail.com>
2019-10-04 16:56:04 +0300

    041019

---
+++ C#Advanced-May-2019/23-WorkShop-BasicAlgorithms/02-RecursiveFactorial/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace _02_RecursiveFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int result = Factorial(input);
+
+            Console.WriteLine(result);
+        }
+
+        static int Factorial(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            int next = Factorial(number - 1);
+            int result = number * next;
+            return result;
+        }
+
+        //static int Factorial5(int number)
+        //{
+        //    if (number == 0)
+        //    {
+        //        return 1;
+        //    }
+
+        //    int next = Factorial4(number - 1);
+        //    int result = number * next;
+        //    return result;
+        //}
+        //static int Factorial4(int number)
+        //{
+        //    if (number == 0)
+        //    {
+        //        return 1;
+        //    }
+
+        //    int next = Factorial3(number - 1);
+        //    int result = number * next;
+        //    return result;
+        //}
+        //static int Factorial3(int number)
+        //{
+        //    if (number == 0)
+        //    {
+        //        return 1;
+        //    }
+
+        //    int next = Factorial2(number - 1);
+        //    int result = number * next;
+        //    return result;
+        //}
+        //static int Factorial2(int number)
+        //{
+        //    if (number == 0)
+        //    {
+        //        return 1;
+        //    }
+
+        //    int next = Factorial1(number - 1);
+        //    int result = number * next;
+        //    return result;
+        //}
+        //static int Factorial1(int number)
+        //{
+        //    if (number == 0)
+        //    {
+        //        return 1;
+        //    }
+
+        //    int next = Factorial0(number - 1);
+        //    int result = number * next;
+        //    return result;
+        //}
+
+        //static int Factorial0(int number)
+        //{
+        //    if (number == 0)
+        //    {
+        //        return 1;
+        //    }
+
+        //    int next = Factorial0(number - 1);
+        //    int result = number * next;
+        //    return result;
+        //}
+    }
+}
---
+++ C#OOP-May-2019/01-WorkingWithAbstraction/02-PointInRectangle/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _02_PointInRectangle
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] rectangleCoordinates = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int topLeftX = rectangleCoordinates[0];
+            int topLeftY = rectangleCoordinates[1];
+
+            Point topLeftPoint = new Point(topLeftX, topLeftY);
+
+            int bottomRightX = rectangleCoordinates[2];
+            int bottomRightY = rectangleCoordinates[3];
+
+            Point bottomRightPoint = new Point(bottomRightX, bottomRightY);
+
+            Rectangle rectangle = new Rectangle(topLeftPoint, bottomRightPoint);
+
+            int inputLines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < inputLines; i++)
+            {
+                int[] pointCoordinates = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                int pointX = pointCoordinates[0];
+                int pointY = pointCoordinates[1];
+
+                Point point = new Point(pointX, pointY);
+
+                Console.WriteLine(rectangle.Contains(point));
+            }
+        }
+    }
+}

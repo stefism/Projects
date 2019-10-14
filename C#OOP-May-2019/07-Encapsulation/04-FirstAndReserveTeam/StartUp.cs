379ce196a1911d4b137692c5501d555471a08fee
Stefan Markov <stef4otm@gmail.com>
2019-10-14 07:19:55 +0300

    141019

---
+++ C#OOP-May-2019/07-Encapsulation/04-FirstAndReserveTeam/StartUp.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+
+            Team team = new Team("SoftUni");
+
+            for (int i = 0; i < lines; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                string firstName = input[0];
+                string lastName = input[1];
+                int age = int.Parse(input[2]);
+                decimal salary = decimal.Parse(input[3]);
+
+                Person person = null;
+                
+                try
+                {
+                    person = new Person(firstName, lastName, age, salary);
+                }
+
+                catch (Exception exception)
+                {
+
+                    Console.WriteLine(exception.Message);
+                }
+
+                if (person != null)
+                {
+                    persons.Add(person);
+                }
+            }
+
+            if (persons != null)
+            {
+                foreach (var person in persons)
+                {
+                    team.AddPlayer(person);
+                }
+            }
+
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players.");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players.");
+
+        }
+    }
+}
---
+++ C#OOP-May-2019/08-Encapsulation-Exercise/02-AnimalFarm/AnimalFarm/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace AnimalFarm
+{
+    using System;
+    using AnimalFarm.Models;
+    
+    public class Program
+    {
+        static Chicken chicken;
+
+        public static void Main()
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            try
+            {
+                chicken = new Chicken(name, age);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+            if (chicken != null)
+            {
+                Console.WriteLine(
+                "Chicken {0} (age {1}) can produce {2} eggs per day.",
+                chicken.Name,
+                chicken.Age,
+                chicken.ProductPerDay);
+            }
+        }
+    }
+}

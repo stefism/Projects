7f1289a6f6c906961d66ad05664eb1695809a8a5
Stefan Markov <stef4otm@gmail.com>
2019-10-20 18:04:47 +0300

    201019

---
+++ C#Advanced-May-2019/Exams/DemoExam17Feb2019/03-Repository/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository
+{
+    public class Person
+    {
+        public Person(string name, int age, DateTime birthdate)
+        {
+            Name = name;
+            Age = age;
+            Birthdate = birthdate;
+        }
+
+        public string Name { get; private set; }
+
+        public int Age { get; private set; }
+
+        public DateTime Birthdate { get; private set; }
+    }
+}

6b7ee0f84c922d5c5e35f14a3247411726014685
Stefan Markov <stef4otm@gmail.com>
2020-02-28 13:28:11 +0200

    280220

---
+++ C# Entity Framework Core-Oct2019/25 Workshop - ASP.NET Core/Web/PetStore.Web/Controllers/PetsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetStore.Web.Controllers
+{
+    public class PetsController : Controller
+    {
+        public string[] All()
+        {
+            return new[] { "Pesho", "Gosho" };
+        }
+    }
+}

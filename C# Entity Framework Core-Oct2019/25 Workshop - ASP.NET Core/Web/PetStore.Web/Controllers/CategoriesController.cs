5530512d395fc90fbf3037b7ae3f62438ff5ffac
Stefan Markov <stef4otm@gmail.com>
2020-03-02 12:26:58 +0200

    020320

---
+++ C# Entity Framework Core-Oct2019/25 Workshop - ASP.NET Core/Web/PetStore.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PetStore.Services;
+using PetStore.Web.Models.Categories;
+
+namespace PetStore.Web.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryService categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
+        public IActionResult All()
+        {
+            var categories = categoryService.All()
+                .Select(csm => new CategoryListingViewModel()
+                {
+                    Id = csm.Id,
+                    Name = csm.Name
+                }).ToArray();
+
+            return View(categories);
+        }
+    }
+}

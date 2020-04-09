af279af04217951cc65c9c0655fa886203c00e51
Stefan Markov <stef4otm@gmail.com>
2020-04-09 21:50:09 +0300

    090420

--- Java Script-Jan-2020/11 DOM/02. Format the Text/solution.js
+++ Java Script-Jan-2020/11 DOM/02. Format the Text/solution.js
@@ -1,3 +1,28 @@
 function solve() {
-  //TODO...
+    let input = document.getElementById('input')
+    let text = input.innerText.split('. ')
+
+    let output = document.getElementById('output')
+
+
+    let paragraph = ''
+    paragraph += text[0] + '. '
+
+    for (let i = 1; i <= text.length; i++) {
+        if(i % 3 === 0){
+            let p = document.createElement('p')
+            p.innerHTML = paragraph
+            output.appendChild(p)
+            paragraph = ''
+        }
+
+        paragraph += text[i] + '. '
+
+        if(i === text.length && paragraph.length > 2){
+            let p = document.createElement('p')
+            p.innerHTML = paragraph
+            output.appendChild(p)
+        }
+    }
+        //debugger
 }

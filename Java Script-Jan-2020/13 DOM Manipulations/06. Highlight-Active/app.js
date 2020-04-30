c4a709f05b62471b022647eb67018af90dbead60
Stefan Markov <stef4otm@gmail.com>
2020-04-30 11:48:41 +0300

    300420

--- Java Script-Jan-2020/13 DOM Manipulations/06. Highlight-Active/app.js
+++ Java Script-Jan-2020/13 DOM Manipulations/06. Highlight-Active/app.js
@@ -1,3 +1,18 @@
 function focus() {
-    console.log('TODO:...');
+    let sections = document.querySelector("body > div")
+    sections.addEventListener('click', e => onFocus(e))
+
+    function onFocus(e) {
+        let currElement = e.target
+
+        if(currElement.tagName === 'INPUT'){
+            let currSection = currElement.parentNode
+            currSection.classList.add('focused')
+            currElement.addEventListener('blur', function () {
+                currSection.classList.remove('focused')
+
+            })
+        }
+        //debugger
+    }
 }

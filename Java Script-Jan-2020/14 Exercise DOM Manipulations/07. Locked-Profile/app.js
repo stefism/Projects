a986e28e87d913b252c63d867e7abcc0a50ea21f
Stefan Markov <stef4otm@gmail.com>
2020-05-03 21:24:20 +0300

    030520

--- Java Script-Jan-2020/14 Exercise DOM Manipulations/07. Locked-Profile/app.js
+++ Java Script-Jan-2020/14 Exercise DOM Manipulations/07. Locked-Profile/app.js
@@ -1,3 +1,20 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let buttons = document.getElementsByTagName('button')
+    Array.from(buttons)
+        .map(b => b.addEventListener
+        ('click', b => processingProfile(b)))
+
+    function processingProfile(b) {
+        let currBtn = b.target
+        let unlock = currBtn.parentNode.children[4]
+        let hiddenFields = currBtn.parentNode.children[9]
+
+        if(unlock.checked === true && currBtn.innerText === 'Show more'){
+            hiddenFields.style.display = 'block'
+            currBtn.innerText = 'Hide it'
+        } else if(unlock.checked === true && currBtn.innerText === 'Hide it'){
+            hiddenFields.style.display = 'none'
+            currBtn.innerText = 'Show more'
+        }
+    }
 }

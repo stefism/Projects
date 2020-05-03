1e3b78a80646510e8219b0b91c21229a64a49448
Stefan Markov <stef4otm@gmail.com>
2020-05-03 17:45:25 +0300

    030520

--- Java Script-Jan-2020/14 Exercise DOM Manipulations/03. Accordion/accordion.js
+++ Java Script-Jan-2020/14 Exercise DOM Manipulations/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+    let extra = document.getElementById('extra')
+    let button = document.getElementsByClassName('button')[0]
+    
+    if(extra.style.display === 'none'){
+        extra.style.display = 'block'
+        button.innerText = 'Less'
+    } else {
+        extra.style.display = 'none'
+        button.innerText = 'More'
+    }
 }

ea01f6f9b368fbea0289cffb2f93b347243f288f
Stefan Markov <stef4otm@gmail.com>
2020-05-03 18:30:03 +0300

    030520

--- Java Script-Jan-2020/14 Exercise DOM Manipulations/05. Notification/scripts.js
+++ Java Script-Jan-2020/14 Exercise DOM Manipulations/05. Notification/scripts.js
@@ -1,3 +1,8 @@
 function notify(message) {
-
+    let notification = document.getElementById('notification')
+    notification.innerText = message
+    notification.style.display = 'block'
+    setTimeout(function () {
+        notification.style.display = 'none'
+    }, 2000)
 }

9512a27bca6bb1ab7a709dce811527265f8c5d26
Stefan Markov <stef4otm@gmail.com>
2020-05-03 14:50:39 +0300

    030520

--- Java Script-Jan-2020/14 Exercise DOM Manipulations/02. Fill-Dropdown/dropdown.js
+++ Java Script-Jan-2020/14 Exercise DOM Manipulations/02. Fill-Dropdown/dropdown.js
@@ -1,4 +1,14 @@
 function addItem() {
     let newItemText = document.getElementById('newItemText')
     let newItemValue = document.getElementById('newItemValue')
+    let menu = document.getElementById('menu')
+
+    let option = document.createElement('option')
+    option.innerText = newItemText.value
+    option.value = newItemValue.value
+
+    menu.appendChild(option)
+
+    newItemText.value = ''
+    newItemValue.value = ''
 }

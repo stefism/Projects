c613609ade6d5028012884d54af795a9f6054afc
Stefan Markov <stef4otm@gmail.com>
2020-05-03 22:32:58 +0300

    030520

--- Java Script-Jan-2020/14 Exercise DOM Manipulations/08. Encode-and-Decode-Messages/app.js
+++ Java Script-Jan-2020/14 Exercise DOM Manipulations/08. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,46 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let encodeBtn = document
+        .getElementsByTagName('button')[0]
+
+    let decodeBtn = document
+        .getElementsByTagName('button')[1]
+
+    let encodeArea = document
+        .getElementsByTagName('textarea')[0]
+
+    let decodeArea = document
+        .getElementsByTagName('textarea')[1]
+
+    encodeBtn.addEventListener('click', encode)
+    decodeBtn.addEventListener('click', decode)
+
+    function encode() {
+        let text = encodeArea.value
+        text = encoderDecoder(text, 'enc')
+        decodeArea.value = text
+        encodeArea.value = ''
+    }
+
+    function decode() {
+        let text = decodeArea.value
+        text = encoderDecoder(text, 'dec')
+        decodeArea.value = text
+    }
+
+    function encoderDecoder(text, operation) {
+        let output = ''
+
+        for (let i = 0; i < text.length; i++) {
+            let currChar = ''
+
+            operation === 'enc'
+                ? currChar = text[i].charCodeAt(0) + 1
+                : currChar = text[i].charCodeAt(0) - 1
+
+            let currCharDecoded = String.fromCharCode(currChar)
+            output += currCharDecoded
+        }
+
+        return output
+    }
 }

400464d8970315c14c18943b079ebf8c5dbc72b8
Stefan Markov <stef4otm@gmail.com>
2020-05-04 13:42:50 +0300

    040520

--- Java Script-Jan-2020/14 Exercise DOM Manipulations/09. Distance-Converter/distanceConverter.js
+++ Java Script-Jan-2020/14 Exercise DOM Manipulations/09. Distance-Converter/distanceConverter.js
@@ -1,3 +1,29 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    let unitsToMeters = {
+        km: 1000,
+        m: 1,
+        cm: 0.01,
+        mm: 0.001,
+        mi: 1609.34,
+        yrd: 0.9144,
+        ft: 0.3048,
+        in: 0.0254
+    }
+
+    let inputDistance = document.getElementById('inputDistance')
+    let outputDistance = document.getElementById('outputDistance')
+
+    let inputUnits = document.getElementById('inputUnits')
+    let outputUnits = document.getElementById('outputUnits')
+
+    let convertBtn = document.getElementById('convert')
+    convertBtn.addEventListener('click', convert)
+
+    function convert() {
+        let from = inputUnits.value
+        let to = outputUnits.value
+        let distanceFrom = +inputDistance.value
+        let distanceTo = (distanceFrom * unitsToMeters[from]) / unitsToMeters[to]
+        outputDistance.value = distanceTo
+    }
 }

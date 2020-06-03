47c043bcbee15e3ce7f35d6e128be392b4ea4c0b
Stefan Markov <stef4otm@gmail.com>
2020-06-03 18:59:32 +0300

    030620

--- C# DataBaseBasic-MS SQL Server-May2020/09 BUILT-IN FUNCTIONS/Exercises.sql
+++ C# DataBaseBasic-MS SQL Server-May2020/09 BUILT-IN FUNCTIONS/Exercises.sql
@@ -100,6 +100,24 @@ WHERE IpAddress LIKE '___.1_%._%.___'
 ORDER BY Username
 
 -- 17. Show All Games with Duration and Part of the Day
+SELECT [Name] AS Game,
+CASE
+	WHEN DATEPART(HOUR, Start) >=0 AND DATEPART(HOUR, Start) < 12 THEN 'Morning'
+	WHEN DATEPART(HOUR, Start) >=12 AND DATEPART(HOUR, Start) < 18 THEN 'Afternoon'
+	WHEN DATEPART(HOUR, Start) >=18 AND DATEPART(HOUR, Start) < 24 THEN 'Evening'
+END AS [Part of the Day],
+CASE
+	WHEN Duration <= 3 THEN 'Extra Short'
+	WHEN Duration BETWEEN 4 AND 6 THEN 'Short'
+	WHEN Duration > 6 THEN 'Long'
+	WHEN Duration IS NULL THEN 'Extra Long'
+END AS [Duration AS Name]
+FROM Games
+ORDER BY [Name], [Duration AS Name], [Part of the Day]
 
-
-SELECT * FROM Users
+-- Part IV – Date Functions Queries
+-- 18. Orders Table
+SELECT ProductName, OrderDate,
+DATEADD(DAY, 3, OrderDate) AS [Pay Due],
+DATEADD(MONTH, 1, OrderDate) AS [Deliver Due]
+FROM Orders

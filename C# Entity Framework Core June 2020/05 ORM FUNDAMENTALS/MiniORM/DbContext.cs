73eb750b61a82242e51ed9e4896215035adbde4b
Stefan Markov <stef4otm@gmail.com>
2020-06-28 16:25:44 +0300

    280620

---
+++ C# Entity Framework Core June 2020/05 ORM FUNDAMENTALS/MiniORM/DbContext.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+
+namespace MiniORM
+{
+	public abstract class DbContext
+    {
+        private readonly DatabaseConnection connection;
+        private readonly Dictionary<Type, PropertyInfo> dbSetProperties;
+
+        internal static readonly Type[] AllowedSqlTypes =
+        {
+            typeof(string), typeof(int), typeof(uint), typeof(long), typeof(ulong), typeof(decimal), typeof(bool), typeof(DateTime)
+        };
+
+        protected DbContext(string connectionString)
+        {
+            connection = new DatabaseConnection(connectionString);
+
+            dbSetProperties = DiscoverDbSets();
+
+            using (new ConnectionManager(connection))
+            {
+                InitializeDbSets();
+            }
+
+            MapAllRelations();
+        }
+
+        public void SaveChanges()
+        {
+            object[] dbSets = dbSetProperties
+                .Select(pi => pi.Value.GetValue(this)).ToArray();
+
+            foreach (IEnumerable<object> dbSet in dbSets)
+            {
+                object[] invalidEntities = dbSet
+                    .Where(entity => !IsObjectValid(entity)).ToArray();
+
+                if (invalidEntities.Any())
+                {
+                    throw new InvalidOperationException($"{invalidEntities.Length} Invalid Entities found in {dbSet.GetType().Name}!");
+                }
+            }
+
+            using (new ConnectionManager(connection))
+            {
+                using (SqlTransaction transaction = connection.StartTransaction())
+                {
+                    foreach (IEnumerable<object> dbSet in dbSets) //TODO: В документа е само IEnumerable тука. Да се види ако нещо не работи.
+                    {
+                        Type dbSetType = dbSet.GetType().GetGenericArguments().First();
+
+                        MethodInfo persistMethod = typeof(DbContext)
+                            .GetMethod("Persist", BindingFlags.Instance | BindingFlags.NonPublic).MakeGenericMethod(dbSetType);
+
+                        try
+                        {
+                            persistMethod.Invoke(this, new object[] { dbSet });
+                        }
+
+                        catch (TargetInvocationException tie)
+                        {
+
+                            throw tie.InnerException;
+                        }
+
+                        catch (InvalidOperationException)
+                        {
+                            transaction.Rollback();
+                            throw;
+                        }
+
+                        catch (SqlException)
+                        {
+                            transaction.Rollback();
+                            throw;
+                        }
+                    }
+
+                    transaction.Commit();
+                }
+            }
+        }
+
+        private bool IsObjectValid(object e)
+        {
+            var validationContext = new ValidationContext(e);
+            var validationErrors = new List<ValidationResult>();
+
+            var validationResult = Validator.TryValidateObject(e, validationContext, validationErrors, true);
+
+            return validationResult;
+        }
+
+        private void MapAllRelations()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void InitializeDbSets()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Dictionary<Type, PropertyInfo> DiscoverDbSets()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ C# Entity Framework Core June 2020/05 ORM FUNDAMENTALS/MiniORM/DbSet.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MiniORM
+{
+    public class DbSet<TEntity> : ICollection<TEntity>
+        where TEntity : class, new()
+    {
+        internal ChangeTracker<TEntity> ChangeTracker { get; set; }
+        internal IList<TEntity> Entities { get; set; }
+
+        internal DbSet(IEnumerable<TEntity> entities)
+        {
+            Entities = entities.ToList();
+
+            ChangeTracker = new ChangeTracker<TEntity>(entities);
+        }
+
+        public int Count => Entities.Count;
+
+        public bool IsReadOnly => Entities.IsReadOnly;
+
+        public void Add(TEntity item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item), "Item cannot be null!");
+            }
+
+            Entities.Add(item);
+
+            ChangeTracker.Add(item);
+        }
+
+        public void Clear()
+        {
+            while (Entities.Any())
+            {            
+                Remove(Entities.First());
+            }
+        }
+
+        public bool Contains(TEntity item) => Entities.Contains(item);
+
+        public void CopyTo(TEntity[] array, int arrayIndex)
+        => Entities.CopyTo(array, arrayIndex);
+
+        public IEnumerator<TEntity> GetEnumerator()
+        {
+            return Entities.GetEnumerator();
+        }
+
+        public bool Remove(TEntity item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item), "Item cannot be null!");
+            }
+
+            bool removed = Entities.Remove(item);
+
+            if (removed)
+            {
+                ChangeTracker.Remove(item);
+            }
+
+            return removed;
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            foreach (var entity in entities.ToArray())
+            {
+                Remove(entity);
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}

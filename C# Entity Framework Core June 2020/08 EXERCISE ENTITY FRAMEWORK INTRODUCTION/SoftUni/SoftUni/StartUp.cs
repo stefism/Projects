52858858024fd4fc698125bdcbc1b2f67ee0ed97
Stefan Markov <stef4otm@gmail.com>
2020-06-30 23:32:18 +0300

    300620

---
+++ C# Entity Framework Core June 2020/08 EXERCISE ENTITY FRAMEWORK INTRODUCTION/SoftUni/SoftUni/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SoftUni
+{
+    using SoftUni.Data;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var db = new SoftUniContext();
+        }
+    }
+}

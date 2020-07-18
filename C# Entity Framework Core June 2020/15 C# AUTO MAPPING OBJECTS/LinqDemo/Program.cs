ad0181a736825f1eabde14d45e0cd64bda431c19
Stefan Markov <stef4otm@gmail.com>
2020-07-18 14:16:26 +0300

    180720

--- C# Entity Framework Core June 2020/15 C# AUTO MAPPING OBJECTS/LinqDemo/Program.cs
+++ C# Entity Framework Core June 2020/15 C# AUTO MAPPING OBJECTS/LinqDemo/Program.cs
@@ -19,13 +19,44 @@ namespace LinqDemo
             MapperConfiguration config = new MapperConfiguration(cfg =>
             {
                 cfg.CreateMap<Artists, ArtistWithCount>();
+                cfg.AddProfile(new SongsToViewModelProfile());
             });
-           
-            var db = new MusicXContext();           
+
+            // .ReverseMap() се ползва когато искаме от вю модел да направиме клас, който после да вкараме в базата. Примерно когато потребителя е въвел нова песен от приложението, тя после за да се вкара в базата трябва да се обърне на клас, който го има в db контекста.
+
+            var db = new MusicXContext();
 
             ArtistWithCount artistViewModel = db.Artists.Where(x => x.Id == 10)
-                .ProjectTo<ArtistWithCount>(config).FirstOrDefault();
-            Print(artistViewModel);           
+                //.Select(x => new ArtistWithCount 
+                //{
+                //    Name = x.Name,
+                //    CreatedOn = x.CreatedOn,
+                //    ModifiedOn = x.ModifiedOn,
+                //    SongArtistsCount = x.SongArtists.Count,
+                //})
+                .ProjectTo<ArtistWithCount>(config).FirstOrDefault(); // Този ред .ProjectTo, спестява всичкото писане на горното и ръчното мапване.
+            Print(artistViewModel);
+
+            var artists = db.Songs.ProjectTo<SongViewModel>(config)
+                .Take(10).ToList();
+            Print(artists);
+            //ReverseMap(config, db);
+        }
+
+        private static void ReverseMap(MapperConfiguration config, MusicXContext db)
+        {
+            //Reverse Map:
+            var input = new SongViewModel
+            {
+                Name = "Test 1234",
+                SourceName = "VBox7"
+            };
+
+            var mapper = config.CreateMapper();
+            var inputSong = mapper.Map<Songs>(input);
+            db.Songs.Add(inputSong);
+            db.SaveChanges();
+            //Reverse Map.
         }
 
         public static void Print(object artists)
---
+++ C# Entity Framework Core June 2020/15 C# AUTO MAPPING OBJECTS/LinqDemo/SongsToViewModelProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using LinqDemo;
+using LinqDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AutoMappingObject
+{
+    public class SongsToViewModelProfile : Profile
+    {
+        public SongsToViewModelProfile()
+        {
+            this.CreateMap<Songs, SongViewModel>()
+                .ForMember(
+                    svm => svm.Artists,
+                    opt => opt.MapFrom(s =>
+                    string.Join(", ", s.SongArtists.Select(x => x.Artist.Name))))
+                .ForMember(
+                    x => x.LastModified,
+                    opt => opt.MapFrom(s =>
+                    s.ModifiedOn ?? s.CreatedOn))// Ако ModifiedOn е null, използвай CreatedOn.
+                .ReverseMap();
+        }
+    }
+}

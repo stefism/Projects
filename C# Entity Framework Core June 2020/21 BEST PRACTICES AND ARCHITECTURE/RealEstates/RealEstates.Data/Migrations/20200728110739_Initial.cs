d89a88cd06564417487d7c5047c76d47c56440b8
Stefan Markov <stef4otm@gmail.com>
2020-07-28 16:52:55 +0300

    Real Estate - PropertiesService.cs

---
+++ C# Entity Framework Core June 2020/21 BEST PRACTICES AND ARCHITECTURE/RealEstates/RealEstates.Data/Migrations/20200728110739_Initial.cs
@@ -0,0 +1,166 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RealEstates.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BuildingTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BuildingTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Districts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Districts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PropertyTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PropertyTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RealEstateProperties",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Size = table.Column<int>(nullable: false),
+                    Floor = table.Column<int>(nullable: true),
+                    TotalNumberOfFloors = table.Column<int>(nullable: true),
+                    Year = table.Column<int>(nullable: true),
+                    DistrictID = table.Column<int>(nullable: false),
+                    Price = table.Column<int>(nullable: false),
+                    PropertyTypeId = table.Column<int>(nullable: false),
+                    BuildingTypeId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RealEstateProperties", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RealEstateProperties_BuildingTypes_BuildingTypeId",
+                        column: x => x.BuildingTypeId,
+                        principalTable: "BuildingTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RealEstateProperties_Districts_DistrictID",
+                        column: x => x.DistrictID,
+                        principalTable: "Districts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_RealEstateProperties_PropertyTypes_PropertyTypeId",
+                        column: x => x.PropertyTypeId,
+                        principalTable: "PropertyTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RealEstatePropertyTags",
+                columns: table => new
+                {
+                    PropertyId = table.Column<int>(nullable: false),
+                    TagId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RealEstatePropertyTags", x => new { x.PropertyId, x.TagId });
+                    table.ForeignKey(
+                        name: "FK_RealEstatePropertyTags_RealEstateProperties_PropertyId",
+                        column: x => x.PropertyId,
+                        principalTable: "RealEstateProperties",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RealEstatePropertyTags_Tags_TagId",
+                        column: x => x.TagId,
+                        principalTable: "Tags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RealEstateProperties_BuildingTypeId",
+                table: "RealEstateProperties",
+                column: "BuildingTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RealEstateProperties_DistrictID",
+                table: "RealEstateProperties",
+                column: "DistrictID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RealEstateProperties_PropertyTypeId",
+                table: "RealEstateProperties",
+                column: "PropertyTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RealEstatePropertyTags_TagId",
+                table: "RealEstatePropertyTags",
+                column: "TagId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RealEstatePropertyTags");
+
+            migrationBuilder.DropTable(
+                name: "RealEstateProperties");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+
+            migrationBuilder.DropTable(
+                name: "BuildingTypes");
+
+            migrationBuilder.DropTable(
+                name: "Districts");
+
+            migrationBuilder.DropTable(
+                name: "PropertyTypes");
+        }
+    }
+}
---
+++ C# Entity Framework Core June 2020/21 BEST PRACTICES AND ARCHITECTURE/RealEstates/RealEstates.Services/DistrictService.cs
@@ -0,0 +1,21 @@
+﻿using RealEstates.Services.Contracts;
+using RealEstates.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealEstates.Services
+{
+    public class DistrictService : IDistrictService
+    {
+        public IEnumerable<DistrictViewModel> GetTopDistrictsByAveragePrice(int count = 10)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<DistrictViewModel> GetTopDistrictsByNumberOfProperties(int count = 10)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

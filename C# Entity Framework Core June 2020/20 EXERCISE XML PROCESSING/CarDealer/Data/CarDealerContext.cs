a3f5502227aad9c35113ddd3513d552d93b1ff53
Stefan Markov <stef4otm@gmail.com>
2020-07-28 11:24:24 +0300

    280720 Creare Real Estate.Sln

--- C# Entity Framework Core June 2020/20 EXERCISE XML PROCESSING/CarDealer/Data/CarDealerContext.cs
+++ C# Entity Framework Core June 2020/20 EXERCISE XML PROCESSING/CarDealer/Data/CarDealerContext.cs
@@ -26,7 +26,7 @@
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer("Server=DESKTOP-LNP1A21\SQLEXPRESS;Database=CarDealer;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer(@"Server=DESKTOP-LNP1A21\SQLEXPRESS;Database=CarDealer;Trusted_Connection=True;");
             }
         }
 
--- C# Entity Framework Core June 2020/20 EXERCISE XML PROCESSING/CarDealer/StartUp.cs
+++ C# Entity Framework Core June 2020/20 EXERCISE XML PROCESSING/CarDealer/StartUp.cs
@@ -1,5 +1,12 @@
-﻿using CarDealer.Data;
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions.Impl;
+using CarDealer.Data;
+using CarDealer.Dtos.Import;
+using CarDealer.Models;
+using System.Collections.Generic;
 using System.IO;
+using System.Xml.Linq;
+using System.Xml.Serialization;
 
 namespace CarDealer
 {
@@ -8,15 +15,63 @@ namespace CarDealer
         public static void Main(string[] args)
         {
             var db = new CarDealerContext();
-            string suppliers = File.ReadAllText("../../../Datasets/suppliers.xml");
 
-            string q09 = ImportSuppliers(db, suppliers);
+            //db.Database.EnsureDeleted();
+            //db.Database.EnsureCreated();
+
+            //string suppliers = File.ReadAllText("../../../Datasets/suppliers.xml");
+            string parts = File.ReadAllText("../../../Datasets/parts.xml");
+
+            //string q09 = ImportSuppliers(db, suppliers);
+            string q10 = ImportParts(db, parts);
+
+            System.Console.WriteLine(q10);
+        }
+
+        public static string ImportParts(CarDealerContext context, string inputXml)
+        //Query 10. Import Parts
+        {
+            var config = new MapperConfiguration(cfg => 
+            { cfg.CreateMap<PartsDto, Part>(); });
+
+            var mapper = config.CreateMapper();
+
+            var serializer = new XmlSerializer(typeof(List<PartsDto>), new XmlRootAttribute("Parts"));
+            
+            var xmlDocument = XDocument.Parse(inputXml);
+            var reader = xmlDocument.CreateReader();
+
+            var partsDto = (List<PartsDto>)serializer.Deserialize(reader);
+            var parts = mapper.Map<List<Part>>(partsDto);
+
+            context.Parts.AddRange(parts);
+            context.SaveChanges();
+
+            return $"Successfully imported {parts.Count}";
         }
 
         public static string ImportSuppliers(CarDealerContext context, string inputXml)
         //Query 09. Import Suppliers
         {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<SupplierDto, Supplier>();
+            });
 
+            IMapper mapper = config.CreateMapper();
+
+            var serializer = new XmlSerializer(typeof(List<SupplierDto>), new XmlRootAttribute("Suppliers"));
+
+            var xmlDocument = XDocument.Parse(inputXml);
+            var reader = xmlDocument.CreateReader();
+
+            var suppliersDto = (List<SupplierDto>)serializer.Deserialize(reader);
+            var suppliers = mapper.Map<List<Supplier>>(suppliersDto);
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Count}";
         }
     }
 }

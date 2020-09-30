b3bdf0effacce4eebfc5c9b7d43d89815665e505
Stefan Markov <stef4otm@gmail.com>
2020-09-30 23:03:20 +0300

    Soft Uni Http Server

---
+++ C# Web Basics - September 2020/06 WORKSHOP/SoftUniServer/Apps/MyFirstMvcApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using SUS.HTTP;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace MyFirstMvcApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            IHttpServer server = new HttpServer();          
+
+            server.AddRoute("/", HomePage);
+            server.AddRoute("/favicon.ico", Favicon);
+            server.AddRoute("/about", About);
+            server.AddRoute("/users/login", Login);
+            await server.StartAsync(80);
+        }
+
+        static HttpResponse HomePage(HttpRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        static HttpResponse Favicon(HttpRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        static HttpResponse About(HttpRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        static HttpResponse Login(HttpRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- C# Web Basics - September 2020/06 WORKSHOP/SoftUniServer/SUS.HTTP/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace SUS.HTTP
-{
-    public class Class1
-    {
-    }
-}

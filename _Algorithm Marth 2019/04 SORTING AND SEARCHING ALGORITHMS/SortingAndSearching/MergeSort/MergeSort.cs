90ff07fd2743824ee6fb13f8d4a2331463979f0f
Stefan Markov <stef4otm@gmail.com>
2021-01-13 23:03:23 +0200

    130121

--- _Algorithm Marth 2019/04 SORTING AND SEARCHING ALGORITHMS/SortingAndSearching/MergeSort/MergeSort.cs
+++ _Algorithm Marth 2019/04 SORTING AND SEARCHING ALGORITHMS/SortingAndSearching/MergeSort/MergeSort.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace MergeSort
 {
@@ -22,16 +23,48 @@ namespace MergeSort
         private static void MergeArray(int[] array, int startIndex, int middleIndex, int endIndex)
         {
             //Заради закръглянето на цепенето на масива, има шанс средния индекс да излезе извън границите, затова трябва да се провери дали е част от масива.
-            if (array[middleIndex] <= array[middleIndex + 1])
+            if (middleIndex < 0 || middleIndex + 1 >= array.Length 
+                || array[middleIndex] <= array[middleIndex + 1])
             {
                 return;
+            }
+
+            int[] helpArr = new int[array.Length];
+
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                helpArr[i] = array[i];
+            }
+
+            int leftIndex = startIndex;
+            int rightIndex = middleIndex + 1;
+
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                if (leftIndex > middleIndex)
+                {
+                    array[i] = helpArr[rightIndex++];
+                }
+                else if (rightIndex > endIndex)
+                {
+                    array[i] = helpArr[leftIndex++];
+                }
+                else if (helpArr[leftIndex] <= helpArr[rightIndex])
+                {
+                    array[i] = helpArr[leftIndex++];
+                }
+                else
+                {
+                    array[i] = helpArr[rightIndex++];
+                }
             }
         }
 
         static void Main(string[] args)
         {
-            var numbers = new[] { 102, 6, 84, 13, 1, 70, 5 };
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
             Sort(numbers, 0, numbers.Length - 1);
+            Console.WriteLine(string.Join(", ", numbers));
         }
     }
 }

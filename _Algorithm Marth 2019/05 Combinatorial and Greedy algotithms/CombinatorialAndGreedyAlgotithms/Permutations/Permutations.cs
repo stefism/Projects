0fa240c330f549874bf50f6f987150c8f66d7786
Stefan Markov <stef4otm@gmail.com>
2021-01-14 00:46:26 +0200

    140121

---
+++ _Algorithm Marth 2019/05 Combinatorial and Greedy algotithms/CombinatorialAndGreedyAlgotithms/Permutations/Permutations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Permutations
+{
+    class Permutations
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

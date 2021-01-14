c4b56fab8b590369dea8bb6ef77c0f143741868a
Stefan Markov <stef4otm@gmail.com>
2021-01-14 11:09:22 +0200

    140121

--- _Algorithm Marth 2019/05 Combinatorial and Greedy algotithms/CombinatorialAndGreedyAlgotithms/Permutations/Permutations.cs
+++ _Algorithm Marth 2019/05 Combinatorial and Greedy algotithms/CombinatorialAndGreedyAlgotithms/Permutations/Permutations.cs
@@ -4,9 +4,37 @@ namespace Permutations
 {
     class Permutations
     {
+        static int[] elements;
+        static bool[] used;
+        static int[] permutations;
+        static void Permute(int index) // index - current cell to fill
+        {
+            if (index >= elements.Length)
+            {
+                Console.WriteLine(string.Join(" ", permutations));
+            }
+            else
+            {
+                for (int i = 0; i < elements.Length; i++)
+                {
+                    if (!used[i])
+                    {
+                        int currentNumber = elements[i];
+                        used[i] = true;
+                        permutations[index] = currentNumber;
+                        Permute(index + 1);
+                        used[i] = false;
+                    }
+                }
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            elements = new[] { 1, 2, 3 };
+            used = new bool[elements.Length];
+            permutations = new int[elements.Length];
+
+            Permute(0);
         }
     }
 }

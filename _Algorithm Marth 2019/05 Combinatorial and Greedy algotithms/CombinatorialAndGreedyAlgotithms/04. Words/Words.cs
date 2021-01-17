8735bbb8a727fc85330c2df1d6b60f33d2536b3f
Stefan Markov <stef4otm@gmail.com>
2021-01-17 15:41:30 +0200

    170121

---
+++ _Algorithm Marth 2019/05 Combinatorial and Greedy algotithms/CombinatorialAndGreedyAlgotithms/04. Words/Words.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Words
+{
+    class Words // Решава се с алгоритъм за пермутация с повторение.
+    {
+        private static char[] elements;
+        
+        private static int count;
+
+        static void Gen(int index)
+        {
+            if (index >= elements.Length)
+            {
+                for (int i = 1; i < elements.Length; i++)
+                {
+                    if (elements[i] == elements[i - 1])
+                    {
+                        return;
+                    }
+                }
+
+                count++;
+            }
+            else
+            {
+                HashSet<int> swapped = new HashSet<int>();
+                for (int i = index; i < elements.Length; i++)
+                {
+                    if (!swapped.Contains(elements[i]))
+                    {
+                        Swap(index, i);
+                        Gen(index + 1);
+                        Swap(index, i);
+                        swapped.Add(elements[i]);
+                    }
+                }
+            }
+        }
+
+        static void Swap(int firstIndex, int secondIndex)
+        {
+            var temp = elements[firstIndex];
+            elements[firstIndex] = elements[secondIndex];
+            elements[secondIndex] = temp;
+        }
+
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            elements = input.ToCharArray();
+
+            bool unique = input.Distinct().Count() == input.Length;
+            //Ако имаме само уникални символи, пермутацията е N! (N фактуриел) и не е нужно да се върти рекурсията. Прави се с цел оптимизация на бързодействието.
+            
+            if (unique)
+            {
+                count = 1;
+                for (int i = 1; i <= input.Length; i++) //пишем <=  защото сме от N фактуриел или  length-а е равно на N.
+                {
+                    count *= i;
+                }
+            }
+            else
+            {
+                Gen(0);
+            }
+          
+            Console.WriteLine(count);
+        }
+    }
+}
--- _Algorithm Marth 2019/05 Combinatorial and Greedy algotithms/CombinatorialAndGreedyAlgotithms/SetCover/SetCover.cs
+++ _Algorithm Marth 2019/05 Combinatorial and Greedy algotithms/CombinatorialAndGreedyAlgotithms/SetCover/SetCover.cs
@@ -1,13 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SetCover
 {
     class SetCover
     {
-        static List<int[]> ChooseSets(IList<int[]> sets, IList<int[]> universe)
+        static List<int[]> ChooseSets(IList<int[]> sets, IList<int> universe)
         {
+            var result = new List<int[]>();
+            var universeSet = new HashSet<int>(universe);
+            var setsSet = new HashSet<int[]>(sets);
 
+            while (universe.Count > 0)
+            {
+                int[] currentSet = setsSet
+                    .OrderByDescending(s => s.Count(s => 
+                    universeSet.Contains(s)))
+                    .First(); //Първият сет да бъде този, който съдържа най-много елементи от universe.
+
+                result.Add(currentSet);
+                setsSet.Remove(currentSet);
+
+                foreach (var number in currentSet)
+                {
+                    universeSet.Remove(number);
+                }
+            }
+
+            return result;
         }
 
         static void Main(string[] args)

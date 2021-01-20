086460b79b7b3f69addd622ba1403980ff061df4
Stefan Markov <stef4otm@gmail.com>
2021-01-20 13:01:01 +0200

    200121

---
+++ _Algorithm Marth 2019/08_DYNAMIC_PROGRAMMING/Fibonacci/Fibonacci.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Fibonacci
+    {
+        static long[] numbers;
+
+        static long Fib(long n)
+        {
+            if (numbers[n] != 0)
+            {
+                return numbers[n];
+            }
+
+            if (n == 1 || n == 2)
+            {
+                return 1;
+            }
+
+            long result = Fib(n - 1) + Fib(n - 2);
+
+            numbers[n] = result;
+
+            return result;
+        }
+
+        static void Main()
+        {
+            numbers = new long[100]; //usualy n+1
+
+            var result = Fib(20);
+            Console.WriteLine(result);
+        }
+    }
+}

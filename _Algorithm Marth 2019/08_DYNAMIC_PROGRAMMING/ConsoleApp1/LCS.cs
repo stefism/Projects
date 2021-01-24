61393f018e61e454a21eb72732726f7b92258bd4
Stefan Markov <stef4otm@gmail.com>
2021-01-24 16:13:36 +0200

    240121

---
+++ _Algorithm Marth 2019/08_DYNAMIC_PROGRAMMING/ConsoleApp1/LCS.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LongestCommonSubsequence
+{
+    class LCS
+    {
+        static void Main()
+        {
+            string firstSequence = Console.ReadLine();
+            string secondSequence = Console.ReadLine();
+
+            var lcs = new int[firstSequence.Length + 1, secondSequence.Length + 1];
+
+            for (int row = 1; row <= firstSequence.Length; row++)
+            {
+                for (int col = 01; col <= secondSequence.Length; col++)
+                {
+                    int upValue = lcs[row - 1, col];
+                    int leftValue = lcs[row, col - 1];
+
+                    int result = Math.Max(upValue, leftValue);
+
+                    if (firstSequence[row - 1] == secondSequence[col - 1])
+                    {
+                        result = Math.Max(1 + lcs[row - 1, col - 1], result);
+                    }
+
+                    lcs[row, col] = result;
+                }
+            }
+
+            Console.WriteLine(lcs[firstSequence.Length, secondSequence.Length]);
+        }
+    }
+}
---
+++ _Algorithm Marth 2019/08_DYNAMIC_PROGRAMMING/SubsetSumWithRepeat/SubsetSumWithRepeat.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace SubsetSumWithRepeat ///With repeat numbers
+{
+    class SubsetSumWithRepeat
+    {
+        static void Main()
+        {
+            var numbers = new[] { 3, 5, 2 };
+
+            var targetSum = 6;
+            
+            var possibleSums = new bool[targetSum + 1];
+            possibleSums[0] = true;
+
+            for (int currPossibleSum = 0; currPossibleSum < possibleSums.Length; currPossibleSum++)
+            {
+                if (possibleSums[currPossibleSum])
+                {
+                    for (int currNumber = 0; currNumber < numbers.Length; currNumber++)
+                    {
+                        var newSum = currPossibleSum + numbers[currNumber];
+                        if (newSum <= targetSum)
+                        {
+                            possibleSums[newSum] = true;
+                        }                        
+                    }
+                }
+            }
+
+            while (targetSum != 0)
+            {
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    var sum = targetSum - numbers[i];
+                    if (sum >=0 && possibleSums[sum])
+                    {
+                        Console.Write(numbers[i] + " ");
+                        targetSum = sum;
+                    }
+                }
+            }
+
+            Console.WriteLine(possibleSums[targetSum]);
+        }
+    }
+}

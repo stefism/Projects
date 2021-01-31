c949dab30e7c847e1e97fad3b10a14dc76716835
Stefan Markov <stef4otm@gmail.com>
2021-01-31 16:33:57 +0200

    310121

---
+++ _Algorithm Marth 2019/11 GRAPHS AND GRAPH ALGORITHMS/PrimsAlgorithm/Edge.cs
@@ -0,0 +1,11 @@
+﻿namespace PrimsAlgorithm
+{
+    class Edge
+    {
+        public int FirstNode { get; set; }
+
+        public int SecondNode { get; set; }
+
+        public int EdgeWeight { get; set; }
+    }
+}

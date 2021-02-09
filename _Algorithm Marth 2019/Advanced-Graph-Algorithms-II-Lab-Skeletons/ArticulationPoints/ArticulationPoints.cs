8c9b2fe962c09a19f76cf41be2a3c101698a83d9
Stefan Markov <stef4otm@gmail.com>
2021-02-09 15:50:26 +0200

    090221

--- _Algorithm Marth 2019/Advanced-Graph-Algorithms-II-Lab-Skeletons/ArticulationPoints/ArticulationPoints.cs
+++ _Algorithm Marth 2019/Advanced-Graph-Algorithms-II-Lab-Skeletons/ArticulationPoints/ArticulationPoints.cs
@@ -3,8 +3,43 @@ using System.Collections.Generic;
 
 public class ArticulationPoints
 {
+    private static List<int>[] graph;
+    private static bool[] visited;
+    private static int[] depths;
+    private static int[] lowpoint;
+    private static int?[] parent;
+
+    private static List<int> articulationPoints;
+
     public static List<int> FindArticulationPoints(List<int>[] targetGraph)
     {
-        throw new NotImplementedException();
+        graph = targetGraph;
+        visited = new bool[targetGraph.Length];
+        depths = new int[targetGraph.Length];
+        lowpoint = new int[targetGraph.Length];
+        parent = new int?[targetGraph.Length];
+        articulationPoints = new List<int>();
+    }
+
+    private static void FindArticulationPoints(int node, int depth)
+    {
+        visited[node] = true;
+        depths[node] = depth;
+        lowpoint[node] = depth;
+
+        int childCount = 0;
+        bool isArticulationPoint = false;
+
+        foreach (var child in graph[node])
+        {
+            if (!visited[child])
+            {
+
+            }
+            else if (child != parent[node])
+            {
+
+            }
+        }
     }
 }
--- _Algorithm Marth 2019/Advanced-Graph-Algorithms-II-Lab-Skeletons/StronglyConnectedComponents/StronglyConnectedComponents.cs
+++ _Algorithm Marth 2019/Advanced-Graph-Algorithms-II-Lab-Skeletons/StronglyConnectedComponents/StronglyConnectedComponents.cs
@@ -1,12 +1,94 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public class StronglyConnectedComponents
 {
+    private static List<int>[] graph;
+    private static List<int>[] reversedGraph;
+    private static bool[] visited;
+
+    private static Stack<int> stack = new Stack<int>();
+
     private static List<List<int>> stronglyConnectedComponents;
 
     public static List<List<int>> FindStronglyConnectedComponents(List<int>[] targetGraph)
     {
-        throw new NotImplementedException();
+        graph = targetGraph;
+        visited = new bool[graph.Length];
+
+        BuildReverseGraph();
+
+        for (int node = 0; node < graph.Length; node++)
+        {
+            if (!visited[node])
+            {
+                Dfs(node);
+            }
+        }
+
+        stronglyConnectedComponents = new List<List<int>>();
+
+        visited = new bool[graph.Length];
+
+        while (stack.Count > 0)
+        {
+            var node = stack.Pop();
+
+            if (!visited[node])
+            {
+                stronglyConnectedComponents.Add(new List<int>());
+                ReverseDfs(node);
+            }
+        }
+
+        return stronglyConnectedComponents;
+    }
+
+    private static void BuildReverseGraph()
+    {
+        reversedGraph = new List<int>[graph.Length];
+
+        for (int node = 0; node < reversedGraph.Length; node++)
+        {
+            reversedGraph[node] = new List<int>();
+        }
+
+        for (int node = 0; node < graph.Length; node++)
+        {
+            foreach (var child in graph[node])
+            {
+                reversedGraph[child].Add(node);
+            }
+        }
+    }
+
+    private static void ReverseDfs(int node)
+    {
+        if (!visited[node])
+        {
+            visited[node] = true;
+            stronglyConnectedComponents.Last().Add(node);
+
+            foreach (var child in reversedGraph[node])
+            {
+                ReverseDfs(child);
+            }
+        }
+    }
+
+    private static void Dfs(int node)
+    {
+        if (!visited[node])
+        {
+            visited[node] = true;
+
+            foreach (var child in graph[node])
+            {
+                Dfs(child);
+            }
+
+            stack.Push(node);
+        }
     }
 }

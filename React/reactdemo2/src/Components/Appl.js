91dd1e8ea057bb7089c2e0288c5b30850ce7676f
Stefan Markov <stef4otm@gmail.com>
2021-02-12 01:14:40 +0200

    120221

---
+++ React/reactdemo2/src/Components/Appl.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Appl(){
+    return(
+        <div>
+            <nav>
+                <h1>Hello again.</h1>
+                <ul>
+                    <li>Task 1</li>
+                    <li>Task 2</li>
+                    <li>Task 3</li>
+                </ul>
+            </nav>
+            <main>
+                <p>Lorem impsum content.</p>
+            </main>
+        </div>
+    );
+}
+
+export default Appl

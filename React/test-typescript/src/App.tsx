60cb6eedb1c0ce47c181decb3131942b30c82c0e
stefism <stef4otm@gmail.com>
2021-02-22 19:08:39 +0200

    220221

--- React/test-typescript/src/App.tsx
+++ React/test-typescript/src/App.tsx
@@ -2,36 +2,56 @@ import React, { useState } from 'react';
 import { fetchQuizQuestions } from './API';
 
 import QuestionCard from './components/QuestionCard';
-import { Difficulty } from './API'
+import { QuestionState, Difficulty } from './API'
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
 
 const TOTAL_QUESTIONS = 10;
 
 function App() {
   const [loading, setLoading] = useState(false);
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [number, setNumber] = useState(0);
-  const [userAnswers, setUserAnswers] = useState([]);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
 
-  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY));
+  // console.log(questions);
 
   const startTrivia = async () => {
+    setLoading(true);
+    setGameOver(false);
 
-  }
+    const newQuestions = await fetchQuizQuestions(
+      TOTAL_QUESTIONS, Difficulty.EASY);
+
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
+  };
 
   const checkAnswer = (event: React.MouseEvent<HTMLButtonElement>) => {
 
-  }
+  };
 
   const nextQuestion = () => {
 
-  }
+  };
 
   return (
     <div className="App">
       <h1>REACT QUIZ</h1>
-      <button className="start" onClick={startTrivia}>Start</button>
+      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+        <button className="start" onClick={startTrivia}>Start</button>
+      ) : null}
+      
       <p className="score">Score: </p>
       <p>Loading Questions ...</p>
       {/* <QuestionCard 

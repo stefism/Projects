b5de4614d5d9fe98c880f6593d22de1949ff35dd
stefism <stef4otm@gmail.com>
2021-03-08 18:01:35 +0200

    080321

--- React/calendar-with-login/src/components/changePricesForm.jsx
+++ React/calendar-with-login/src/components/changePricesForm.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import ChangePrices from '../components/changePrices'
+import { Button } from 'react-bootstrap';
+import '../app/calendar/styles.css'
 
 function ChangePricesForm() {
     const [workDaysPrice, setWorkDaysPrice] = useState(0);
@@ -18,22 +20,22 @@ function ChangePricesForm() {
     }
 
     return (
-        <div>
+        <div class='change-prices'>
+            
             <h3>Change prices</h3>
 
-            <form class='change-prices' onSubmit={handleSubmit}>
-            <label for='work-days'>Work days: </label>
-            <input value={workDaysPrice} onChange={handleChangeWorkDays}
+            <form onSubmit={handleSubmit}>
+            <label class='input-field' for='work-days'>Work days: </label>
+            <input class='input-field' value={workDaysPrice} onChange={handleChangeWorkDays}
             name='work-days' type='number' step="any"/>
             
-            <br/>
-            <label for='week-days'>Week days: </label>
-            <input value={weekDaysPrice} onChange={handleChangeWeekDays}
+            <label class='input-field' for='week-days'>Week days: </label>
+            <input class='input-field' value={weekDaysPrice} onChange={handleChangeWeekDays}
             name='week-days' type='number' step="any"/>
             
-            <br/>
-            <button>Change</button>
+            <button class='btn-submit'>Change</button>
             </form>
+            
         </div>
     )
 }

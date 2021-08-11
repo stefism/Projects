276d5dc42b293b4d2445306878623cb580098745
Stefan Markov <stefan.markov@siriussoftware.bg>
2021-08-11 14:36:40 +0300

    110821

--- 08 ReactJS - Mart 2021/08 Workshop Single-Page Application/my-pets/src/components/Demo/DemoPage.js
+++ 08 ReactJS - Mart 2021/08 Workshop Single-Page Application/my-pets/src/components/Demo/DemoPage.js
@@ -1,8 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import useFetch from "../Hooks/useFetch";
 
 const DemoPage = () => {
     const [count, setCount] = useState(0);
     const [step, setStep] = useState(1);
+    // const [character, setCharacter] = useState({name: 'Nobody'});
+    const [character2, isCharacterLoading] = useFetch(`https://swapi.dev/api/people/${step}`);
+
+    // useEffect(() => {
+    //     fetch(`https://swapi.dev/api/people/1`)
+    //     .then(res => res.json())
+    //     .then(result => setCharacter(result));
+    // }, []); //[] dependency array. Ако го няма, кода ще се изпълнява всеки път, когато нещо се промени в DOM. В случая даже ще завърти безкраен цикъл.
+
+    // useEffect(() => {
+    //     fetch(`https://swapi.dev/api/people/${step}`)
+    //     .then(res => res.json())
+    //     .then(result => setCharacter(result));
+    // }, [step]); // Това е равносилно на ComponentDidUpdate в class компонентите. В случая ще се изпълнява всеки път когато (ако) step се промени.
+
+    useEffect(() => {
+        return () => {
+            console.log('ComponentWillUnmount'); // Трябва да върнем друга функция и това, което е в ретърна ще се изпълни при WillUnmount. ComponentWillUnmount - примерно когато напуснем страницата.
+        }
+    }, []);
 
     const onCounterButtonClickHandler = () => {
         // setCount(count + 1); Не е добре да се прави така. Ще има проблем с race conditions после. Прави се с функция.
@@ -17,7 +38,7 @@ const DemoPage = () => {
 
     return (
         <div>
-            <h1>Counter</h1>
+            <h1>{!isCharacterLoading ? character2?.name : "Loading..."} Counter</h1>
             <h3>{count}</h3>
             <button onClick={onCounterButtonClickHandler}>Increase</button>
             <label htmlFor="step"> Step </label>

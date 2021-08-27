aff154327255810e2229025ba3e73c419d80d705
Stefan Markov <stefan.t.markov@gmail.com>
2021-08-27 21:16:32 +0300

    Edit Pet Details

--- 08 ReactJS - Mart 2021/08 Workshop Single-Page Application/my-pets/src/components/Pet/EditPetDetails.js
+++ 08 ReactJS - Mart 2021/08 Workshop Single-Page Application/my-pets/src/components/Pet/EditPetDetails.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
-import * as petService from '../../Services/PetsService';
 import InputError from '../Shared/InputError';
+import firebase from 'firebase';
 
 const EditPetDetails = ({match, history}) => {
     const [pet, setPet] = useState({});
@@ -8,22 +8,42 @@ const EditPetDetails = ({match, history}) => {
     const [buttonDisabled, setButtonDisabled] = useState(true);
 
     useEffect(() => {
-        petService.getOne(match.params.petId)
-        .then(res => setPet(res));
+        var db = firebase.firestore();
+        var currentPet = db.collection('pets').doc(match.params.petId);
+
+        currentPet.get().then((doc) => {
+            var pet = {
+                id: doc.id,
+                category: doc.get('category'),
+                description: doc.get('description'),
+                imageURL: doc.get('imageURL'),
+                likes: doc.get('likes'),
+                name: doc.get('name'),
+                user: doc.get('user')
+            }
+
+            setPet(pet);
+        });
     }, [match]);
 
     const onDescriptionSaveSubmit = (e) => {
         e.preventDefault();
-        console.log(e.target);
 
-        let petId = match.params.petId;
-        let updatedPet = {...pet, description: e.target.description.value};
+        console.log(e.target.petName.value);
 
-        petService.update(petId, updatedPet)
-        .then(() => {
-            history.push(`/pets/details/${petId}`);
-            return;
-        });
+        let updatedPet = {...pet,
+            name: e.target.petName.value,
+            description: e.target.description.value,
+            imageURL: e.target.imageURL.value
+        };
+
+        console.log(updatedPet);
+
+        // petService.update(petId, updatedPet)
+        // .then(() => {
+        //     history.push(`/pets/details/${petId}`);
+        //     return;
+        // });
     };
 
     const onDescriptionChangeHandler = (e) => {
@@ -46,19 +66,38 @@ const EditPetDetails = ({match, history}) => {
 
     return (
         <section className="detailsMyPet">
-                <h3>{pet.name}</h3>
                 <p>Pet counter: <i className="fas fa-heart"></i> {pet.likes}</p>
                 <p className="img"><img src={pet.imageURL} alt="" /></p>
                 <form onSubmit={onDescriptionSaveSubmit}>
-                    <textarea type="text" 
+                    <p style={{marginBottom: 1}}><b>Name:</b></p>
+                    <h3 style={{marginTop: 10}}>
+                    <textarea
+                    name="petName"
+                    type="text"
+                    style={{textAlign: "center", width: 400, marginTop: 1}}
+                    defaultValue={pet.name}>
+                    </textarea>
+                    </h3>
+                    
+                    <p><b>Description:</b></p>
+                    <textarea style={{width: 400}} type="text" 
                     name="description"
                     onBlur={onDescriptionChangeHandler}
                     onChange={onChangeHandler}
                     defaultValue={pet.description}>
-
                     </textarea>
+
+                    <p><b>Image URL:</b></p>
+                    <textarea type="text" 
+                    name="imageURL"
+                    onChange={onChangeHandler}
+                    defaultValue={pet.imageURL}>
+                    </textarea>
+
                     <InputError>{errorMessage}</InputError>
-                    <button disabled={buttonDisabled} className={buttonDisabled ? "disabled" : "button"}>Save</button>
+
+                    <button  className="button">Edit</button>
+                    <button className="button">Delete Pet</button>
                 </form>
         </section>
     );

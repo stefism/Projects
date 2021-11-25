02a18a957116f5cd866d02ed6c48bb45cd784b7e
Stefan Markov <stefan.t.markov@gmail.com>
2021-11-25 14:54:22 +0200

    25-Nov-21

--- JS Advanced-May 2021/03-Objects and Composition/06_e-StoreCatalogue.js
+++ JS Advanced-May 2021/03-Objects and Composition/06_e-StoreCatalogue.js
@@ -3,25 +3,96 @@ function storeCatalogue(input){
 
     input.forEach(element => {
         let firstLetter = element.charAt(0);
-        console.log(result[firstLetter]);
-        if (result.some(r => r[firstLetter] == firstLetter)) {
-            console.log(result[firstLetter]);
+        if (result[firstLetter]) {
+            result[firstLetter].push(element);
         }
         else {
-            let curr = {
-                [firstLetter]: [element]
-            }
-            result.push(curr);
+            result[firstLetter] = [element]
         }
     });
 
-    let keys = Object.keys(result);
-    console.log(keys);
+    for(let item in result){
+        console.log(item, result[item]);
+    }
+
+    let sort = result.sort((a, b) => a.localeCompare(b));
+
+    console.log(sort);
+}
+
+function solve(input) {
+    let items = [];
+
+    input.forEach(element => {
+        let [name, price] = element.split(" : ");
+        
+        let curr = {
+            name,
+            price
+        }
+        
+        items.push(curr);
+    });
+
+    items.sort((a, b) => a.name.localeCompare(b.name));
+    
+    let result = '';
+    let firstLetter = items[0].name.charAt(0);
+    result += `${firstLetter}\n`;
+
+    items.forEach(element => {
+        let currFirstLetter = element.name.charAt(0);
+
+        if(currFirstLetter != firstLetter) {
+            result += `${currFirstLetter}\n`;
+            firstLetter = currFirstLetter;
+        }
+
+        result += `${element.name}: ${element.price}\n`;
+    });
 
     console.log(result);
 }
 
-storeCatalogue([
+function solve2(input){
+    let dictionary ={}; //Като напишеш обекта и новото му проперти, то автоматично се добавя към обекта.
+    while (input.length) {
+        let [name, price] = input.shift().split(' : ');
+        const firstLetter = name[0];
+
+        if (!dictionary[firstLetter]) {
+            dictionary[firstLetter] = [];
+        }
+
+        dictionary[firstLetter].push({name, price});
+        dictionary[firstLetter].sort((a, b) => (a.name).localeCompare(b.name));
+    }
+
+    let result =[];
+
+    // for(const letter in dictionary){
+    //     let values = dictionary[letter].map(entry => `  ${entry.name}: ${entry.price}`);
+    //     let string = `${letter}\n${values.join('\n')}`;
+    //     result.push(string);
+    // }
+
+    // Object.entries -> [['key', value]]
+    Object.entries(dictionary)
+        .sort((a, b) => a[0].localeCompare(b[0])) //Тук сортираме по заглавната буква.
+        .forEach(entry =>{
+            let values = entry[1] //Тук взимаме стойностите към главната буква.
+            .sort((a, b) => (a.name).localeCompare(b.name))
+            .map(product => `  ${product.name}: ${product.price}`)
+            .join('\n');
+
+            let string = `${entry[0]}\n${values}`;
+            result.push(string);
+        });
+
+    console.log(result.join('\n'));
+}
+
+solve2([
 'Appricot : 20.4',
 'Anti-Bug Spray : 15',
 'Fridge : 1500',
---
+++ JS Advanced-May 2021/03-Objects and Composition/07_e-TownsToJson.js
@@ -0,0 +1,53 @@
+function townsToJson(input){
+    let result =[];
+    let [properties, ...towns] = input;
+    let [townProp, latitudeProp, longitudeProp] = properties.split(" | ");
+    townProp = townProp.substring(2);
+    longitudeProp = longitudeProp.slice(0, -2);
+
+    towns.forEach(element => {
+        let [town, latitude, longitude] = element.split(" | ");
+        town = town.substring(2);
+        longitude = longitude.slice(0, -2);
+
+        let currTown = {
+            [townProp]: town,
+            [latitudeProp]: Number(Number(latitude).toFixed(2)),
+            [longitudeProp]: Number(Number(longitude).toFixed(2))
+        }
+
+        result.push(currTown);
+    });
+
+    console.log(JSON.stringify(result));
+}
+
+function townsToJson2(input){
+    let[columns, ...table] = input.map(splitLine);
+
+    function isEmptyString(x) {
+        return x !== "";
+    }
+
+    function convertIfNum(x) {
+        return isNaN(x) ? x : Number(Number(x).toFixed(2));
+    }
+
+    function splitLine(input) {
+        return input.split(' | ')
+            .filter(isEmptyString)
+            .map(x => x.trim())
+            .map(convertIfNum);
+    }
+
+    return JSON.stringify(table.map(entry => {
+        return columns.reduce((acc, curr, index) => {
+            acc[curr] = entry[index];
+            return acc;
+        }, {});
+    }));
+}
+
+townsToJson(['| Town | Latitude | Longitude |',
+'| Sofia | 42.696552 | 23.32601 |',
+'| Beijing | 39.913818 | 116.363625 |']);

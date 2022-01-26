c8299d62c6b6076ae2f63291be4e783a93e5c2ba
Stefan Markov <stefan.t.markov@gmail.com>
2022-01-26 09:37:00 +0200

    260122

--- JS Advanced-May 2021/12-Classes and Attributes - Exercise/08-Juice-flavors.js
+++ JS Advanced-May 2021/12-Classes and Attributes - Exercise/08-Juice-flavors.js
@@ -1,5 +1,6 @@
 function juiceFlavours(inputArray) {
     let bottles = {};
+    let output = {};
 
     inputArray.forEach(juice => {
         let [name, quantity] = juice.split(' => ');
@@ -8,6 +9,31 @@ function juiceFlavours(inputArray) {
             bottles[name] = 0;
         }
 
-        bottles[name] += quantity;
-    })
+        bottles[name] += Number(quantity);
+
+        if(bottles[name] >= 1000) {
+            let bottlesNumber = bottles[name] / 1000;
+            let [bottlesMake, remainder] = bottlesNumber.toString().split('.');
+
+            if(!output[name]) {
+                output[name] = Number(bottlesMake);
+            } else {
+                output[name] += Number(bottlesMake);
+            }
+            
+            bottles[name] = Number(remainder);
+        }
+    });
+
+    for(const fruit in output) {
+        console.log(`${fruit} => ${output[fruit]}`);
+    }
 }
+
+juiceFlavours(['Orange => 2000',
+'Peach => 1432',
+'Banana => 450',
+'Peach => 600',
+'Strawberry => 549',
+'Orange => 1020']
+);

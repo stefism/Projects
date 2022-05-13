fdfe2683d75b2a52d15db5a31095cdc118f90fa0
Stefan Markov <stefan.t.markov@gmail.com>
2022-05-13 16:16:39 +0300

    130522

--- JS Applications-Oct-2021/08-Routing-Exercise/01.Furniture/src/app.js
+++ JS Applications-Oct-2021/08-Routing-Exercise/01.Furniture/src/app.js
@@ -1,4 +1,6 @@
+import { logout } from './api/data.js';
 import { page, render } from './lib.js';
+import { getUserData } from './util.js';
 import { catalogPage } from './views/catalog.js';
 import { createPage } from './views/create.js';
 import { detailsPage } from './views/details.js';
@@ -7,10 +9,12 @@ import { loginPage } from './views/login.js';
 import { registerPage } from './views/register.js';
 
 //Ако искаме да си тестваме функциите през конзолата на браузъра, пишем тези два реда.
-import * as api from './api/data.js';
-window.api = api;
+// import * as api from './api/data.js';
+// window.api = api;
 
 const root = document.querySelector('div.container');
+
+document.getElementById('logoutBtn').addEventListener('click', onLogout);
 
 page(decorateContext); //Така, това ще се изпълни винаги преди останалите. Това е мидълуер.
 
@@ -22,10 +26,31 @@ page('/login', loginPage);
 page('/register', registerPage);
 page('/my-furniture', catalogPage);
 
+updateUserNav();
 page.start();
 
 function decorateContext(context, next) {
     context.render = (content) => render(content, root);
+    context.updateUserNav = updateUserNav;
 
     next(); //Задължително, иначе тези след него няма да се изпълнят.
+}
+
+function updateUserNav() {
+    const userData = getUserData();
+
+    if(userData) {
+        document.getElementById('user').style.display = 'inline-block';
+        document.getElementById('guest').style.display = 'none';
+    } else {
+        document.getElementById('user').style.display = 'none';
+        document.getElementById('guest').style.display = 'inline-block';
+    }
+}
+
+async function onLogout() {
+    await logout();
+    updateUserNav();
+
+    page.redirect('/');
 }
--- JS Applications-Oct-2021/08-Routing-Exercise/01.Furniture/src/views/login.js
+++ JS Applications-Oct-2021/08-Routing-Exercise/01.Furniture/src/views/login.js
@@ -1,3 +1,51 @@
-export function loginPage() {
-    console.log('login view');
+import { login } from '../api/data.js';
+import { html } from '../lib.js';
+
+const loginTemplate = (onSubmit, errorMsg) => html`
+<div class="row space-top">
+    <div class="col-md-12">
+        <h1>Login User</h1>
+        <p>Please fill all fields.</p>
+    </div>
+</div>
+<form @submit=${onSubmit}>
+    <div class="row space-top">
+        <div class="col-md-4">
+            ${errorMsg ? html`<div class="form-group error">${errorMsg}</div>` : null}
+            <div class="form-group">
+                <label class="form-control-label" for="email">Email</label>
+                <input class=${'form-control' + (errorMsg ? ' is-invalid' : '')} id="email" type="text" name="email">
+            </div>
+            <div class="form-group">
+                <label class="form-control-label" for="password">Password</label>
+                <input class=${'form-control' + (errorMsg ? ' is-invalid' : '')} id="password" type="password" name="password">
+            </div>
+            <input type="submit" class="btn btn-primary" value="Login" />
+        </div>
+    </div>
+</form>
+`;
+
+export function loginPage(context) {
+    context.render(loginTemplate(onSubmit));
+
+    async function onSubmit(event) {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+
+        const email = formData.get('email');
+        const password = formData.get('password');
+
+        try {
+            await login(email, password);
+
+            context.updateUserNav();
+            context.page.redirect('/');
+        } catch (error) {
+            context.render(loginTemplate(onSubmit, error));
+        }
+
+        
+    }
 }
